47900e78c9a02d3c71fd45288d15e9f5cd7025b6
Roar Thronæs <roart@nvg.ntnu.no>
2015-12-05 18:19:26 +0100

    change name to period. null checks

--- db/src/main/java/roart/model/Stock.java
+++ db/src/main/java/roart/model/Stock.java
@@ -4,23 +4,33 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 //import org.hibernate.annotations.Index;
 
+
+
+
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "Stock")
 @org.hibernate.annotations.Table(appliesTo = "Stock")
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-public class Stock implements Serializable {
+public class Stock implements Serializable /*,Comparable<Stock>*/ {
 
 	@Id
 @Column
 private String dbid;
+@Column
+private String marketid;
 @Column
 private String id;
 @Column
@@ -32,13 +42,15 @@ private Double price;
 @Column
 private String currency;
 @Column
-private Double day;
+private Double period1;
 @Column
-private Double month;
+private Double period2;
 @Column
-private Double week;
+private Double period3;
 @Column
-private Double thisyear;
+private Double period4;
+@Column
+private Double period5;
 
 /*
 	public Stock(String dbid, String id, String name, Date date, Double kurs, String currency, Double day, Double week, Double month, Double sofaryear) {
@@ -69,6 +81,14 @@ public String getId() {
 
 public void setId(String id) {
 	this.id = id;
+}
+
+public String getMarketid() {
+    return marketid;
+}
+
+public void setMarketid(String marketid) {
+    this.marketid = marketid;
 }
 
 public String getName() {
@@ -103,38 +123,47 @@ public void setCurrency(String currency) {
 	this.currency = currency;
 }
 
-public Double getDay() {
-	return day;
+public Double getPeriod1() {
+	return period1;
 }
 
-public void setDay(Double day) {
-	this.day = day;
+public void setPeriod1(Double period1) {
+	this.period1 = period1;
 }
 
-public Double getMonth() {
-	return month;
+public Double getPeriod2() {
+    return period2;
 }
 
-public void setMonth(Double month) {
-	this.month = month;
+public void setPeriod2(Double period2) {
+    this.period2 = period2;
 }
 
-public Double getWeek() {
-	return week;
+public Double getPeriod3() {
+    return period3;
 }
 
-public void setWeek(Double week) {
-	this.week = week;
+public void setPeriod3(Double period3) {
+    this.period3 = period3;
 }
 
-public Double getThisyear() {
-	return thisyear;
+public Double getPeriod4() {
+    return period4;
 }
 
-public void setThisyear(Double thisyear) {
-	this.thisyear = thisyear;
+public void setPeriod4(Double period4) {
+    this.period4 = period4;
 }
 
+public Double getPeriod5() {
+    return period5;
+}
+
+public void setPeriod5(Double period5) {
+    this.period5 = period5;
+}
+
+@Transient
 	public static Stock ensureExistence(String dbid) throws Exception {
         Stock fi = getByDbid(dbid);
         if (fi == null) {
@@ -145,13 +174,36 @@ public void setThisyear(Double thisyear) {
         return fi;
     }
 
-    public static Stock getByDbid(String dbid) throws Exception {
+    @Transient
+        public static Stock getByDbid(String dbid) throws Exception {
         //return (Stock) HibernateUtil.getHibernateSession().createQuery("from Stock where dbid = :dbid").setParameter("dbid", dbid).uniqueResult();
         // this is slower:                                                  
         return (Stock) HibernateUtil.getHibernateSession().get(Stock.class, dbid);                                           
     }
 
-    public static List<Stock> getAll() throws Exception {
+    @Transient
+        public static List<Stock> getAll() throws Exception {
         return HibernateUtil.convert(HibernateUtil.currentSession().createQuery("from Stock").list(), Stock.class);
     }
+
+    @Transient
+       public static List<String> getMarkets() throws Exception {
+        return (List<String>) HibernateUtil.getHibernateSession().createQuery("select distinct (marketid) from Stock").list() ;
+    }
+
+    @Transient
+        public static List<Stock> getAll(String mymarket) throws Exception {
+        return (List<Stock>) HibernateUtil.convert(HibernateUtil.currentSession().createQuery("from Stock where marketid = :mymarket").setParameter("mymarket",  mymarket).list(), Stock.class);
+    }
+
+    /*
+    @Transient
+    public int compareTo(Stock arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    */
+    
+    //@Transient
+
 }
