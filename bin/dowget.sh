68e5f9a585e9b853ba27de02444eaf556cec26dc
Roar Thronæs <roart@nvg.ntnu.no>
2015-12-05 18:20:54 +0100

    new and updated scripts

--- bin/dowget.sh
+++ bin/dowget.sh
@@ -2,3 +2,5 @@
 cd /tmp
 mv sok.html\?sok\=1 sok.html\?sok\=1.old
 wget "https://www.nordnet.no/mux/web/fonder/sok.html?sok=1"
+mv indexoverview.aspx indexoverview.aspx.old
+wget http://www.morningstar.no/no/tools/indexoverview.aspx
