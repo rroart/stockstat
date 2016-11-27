82cb53a608d58055e1b7506c0dc2920139a416a0
Roar Thronæs <roart@nvg.ntnu.no>
2016-11-27 20:09:59 +0100

    new jetty. go back to older spark, because it depended on new jersey and broke jetty

--- web/src/main/java/roart/db/DbSpark.java
+++ web/src/main/java/roart/db/DbSpark.java
@@ -14,8 +14,9 @@ import java.io.StringReader;
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.DataFrame;
+// spark2: import org.apache.spark.sql.SparkSession;
+// spark2: import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -33,10 +34,34 @@ public class DbSpark {
     
     public DbSpark() {
 	try {
-            SparkSession spark = SparkSession.builder().master("local[*]").appName("Stockstat Spark").getOrCreate();
+	    String sparkmaster = "spark://127.0.0.1:7077";
+	    SparkConf sparkconf = new SparkConf();
+ 	    String master = sparkmaster;
+	    sparkconf.setMaster(master);
+	    sparkconf.setAppName("stockstat");
+	    // it does not work well with default snappy
+	    sparkconf.set("spark.io.compression.codec", "lzf");
+	    sparkconf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+	    //sparkconf.set("spark.kryo.registrator", "org.apache.mahout.sparkbindings.io.MahoutKryoRegistrator");
+	    String userDir = System.getProperty("user.dir");
+	    log.info("user.dir " + userDir);
+	    String[] jars = {
+		                    "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/mahout-spark_2.10-0.12.0.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/mahout-hdfs-0.12.0.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/mahout-math-0.12.0.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/guava-16.0.1.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/fastutil-7.0.11.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT/WEB-INF/lib/mahout-math-scala_2.10-0.12.0.jar",
+				                        "file:" + userDir + "/target/stockstat-web-0.4-SNAPSHOT.jar"
+	    };
+	    // first try without sparkconf.setJars(jars);
+	    
+	    JavaSparkContext jsc = new JavaSparkContext(sparkconf);
+            SQLContext sqlContext = new SQLContext(jsc);
+	    DataFrame df = sqlContext.sql("select * from meta");
+	    // spark2:            SparkSession spark = SparkSession.builder().master("local[*]").appName("Stockstat Spark").getOrCreate();
             
-            //SQLContext sqlContext = new SQLContext(jsc);
-            Dataset<Row> df = spark.read().jdbc("jdbc:postgresql://stockstat:password@localhost:5432/stockstat", "meta", null);
+            // spark2: Dataset<Row> df = spark.read().jdbc("jdbc:postgresql://stockstat:password@localhost:5432/stockstat", "meta", null);
 	} catch (Exception e) {
 	    log.error(Constants.EXCEPTION, e);
 	}
