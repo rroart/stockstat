1b1f8556052c28095103782569c6672acfcb8fe5
Roar Thronæs <roart@nvg.ntnu.no>
2017-03-17 06:57:28 +0100

    cleanup

--- web/src/main/java/roart/util/SvgUtil.java
+++ web/src/main/java/roart/util/SvgUtil.java
@@ -14,7 +14,6 @@ import java.io.Writer;
 import org.apache.batik.dom.GenericDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
@@ -24,18 +23,8 @@ import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 
 import roart.client.MyVaadinUI;
-import roart.service.ControlService;
-
-import com.vaadin.server.ExternalResource;
-import com.vaadin.server.Resource;
-import com.vaadin.server.Sizeable;
 import com.vaadin.server.StreamResource;
 import com.vaadin.server.StreamResource.StreamSource;
-import com.vaadin.ui.BrowserFrame;
-import com.vaadin.ui.Embedded;
-import com.vaadin.ui.Image;
-import com.vaadin.ui.Layout;
-import com.vaadin.ui.VerticalLayout;
 
 public class SvgUtil {
 
@@ -101,47 +90,6 @@ public class SvgUtil {
         outputStream.flush();
         outputStream.close();
         return outputStream;
-    }
-
-    public static void bla2(Layout layout, Resource res) {
-        //Resource res = new ThemeResource("images/pygame_icon.svg");
-
-        // Display the object
-        System.out.println("bla " + res.toString());
-        Embedded object = new Embedded("My SVG", res);
-        object.setMimeType("image/svg+xml"); // Unnecessary
-        //object.setHeight(480, Sizeable.Unit.PIXELS );
-        //object.setWidth(800, Sizeable.Unit.PIXELS );
-        System.out.println("isze" + object.getWidth() + " " + object.getHeight());
-        System.out.println("tostr "+object.getSource().getMIMEType());
-        layout.addComponent(object);
-    }
-
-    public static void bla3(Layout layout, Resource res) {
-        BrowserFrame embedded = new BrowserFrame("SVG", res);
-        //embedded.setSizeFull();
-        //embedded.setHeight(480, Sizeable.Unit.PIXELS );
-        //embedded.setWidth(800, Sizeable.Unit.PIXELS );
-        //embedded.setHeight("200");
-        embedded.setVisible(true);
-        System.out.println("isze" + embedded.getWidth() + " " + embedded.getHeight());
-        layout.addComponent(embedded);
-    }
-
-    public static void bla4(VerticalLayout tab) {
-        ExternalResource img = new ExternalResource ("http://vignette2.wikia.nocookie.net/farscape/images/0/04/Moya1.jpg");
-        Embedded image = new Embedded("1", img);
-        System.out.println("isze" + image.getWidth() + " " + image.getHeight());
-        tab.addComponent(image);
-    }
-
-    public static void bla5(Layout tab,Resource res) {
-        Image image = new Image ("Image", res);
-        //Embedded image = new Embedded("1", img);
-        image.setHeight(480, Sizeable.Unit.PIXELS );
-        image.setWidth(800, Sizeable.Unit.PIXELS );
-        System.out.println("isze" + image.getWidth() + " " + image.getHeight());
-        tab.addComponent(image);
     }
 
     /**
