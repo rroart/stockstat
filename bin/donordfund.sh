927ae10c04bfc8119c7724731dc76f7cd4f54304
Roar Thronæs <roart@nvg.ntnu.no>
2017-04-05 18:51:16 +0200

    remove local

--- bin/donordfund.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-DIRNAME=`dirname $0`
-$DIRNAME/nordfund.sh
-JAVA_HOME=/opt/jdk8 java -jar $DIRNAME/../input/target/stockstat-input-0.4-SNAPSHOT-jar-with-dependencies.jar /tmp/nordfund.xml
--- bin/dowget.sh
+++ bin/dowget.sh
@@ -12,14 +12,6 @@ DIRNAME=`dirname $0`
 mkdir $DIRNAME/$DATE
 mv /tmp/*.xml $DIRNAME/$DATE
 fi
-mv sok.html\?sok\=1 sok.html\?sok\=1.old
-wget "https://www.nordnet.no/mux/web/fonder/sok.html?sok=1"
-mv sok.html\?sok\=1\&flik\=historik sok.html\?sok\=1\&flik\=historik.old
-wget "http://www.nordnet.no/mux/web/fonder/sok.html?sok=1&flik=historik"
-mv indexoverview.aspx indexoverview.aspx.old
-wget http://www.morningstar.no/no/tools/indexoverview.aspx
-mv categoryoverview.aspx categoryoverview.aspx.old
-wget http://www.morningstar.no/NO/tools/categoryoverview.aspx
 mv introduction.aspx introduction.aspx.old
 wget http://www.cboe.com/micro/volatility/introduction.aspx
 #mv kitco-gold-index.html kitco-gold-index.html.old
