26184434d0f2bb7d346265d35e756d82e4027e1a
Roar Thronæs <roart@nvg.ntnu.no>
2017-04-16 16:54:19 +0200

    move to common

--- common/config/src/main/java/roart/config/ConfigConstants.java
+++ common/config/src/main/java/roart/config/ConfigConstants.java
@@ -5,4 +5,5 @@ public class ConfigConstants {
     public static final String SPARK = "spark";
     public static final String HIBERNATE = "hibernate";
     public static final String SPARKMASTER = "sparkmaster";
+    public static final String[] dbvalues = { HIBERNATE, SPARK };
 }
