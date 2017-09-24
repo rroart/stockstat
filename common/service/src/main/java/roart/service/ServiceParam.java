0d6b4c043b539b728c1481eacca00443a2497b06
Roar Thronæs <roart@nvg.ntnu.no>
2017-09-24 09:50:39 +0200

    Indent.

--- common/service/src/main/java/roart/service/ServiceParam.java
+++ common/service/src/main/java/roart/service/ServiceParam.java
@@ -6,8 +6,9 @@ import roart.config.MyConfig;
 import roart.model.GUISize;
 
 public class ServiceParam {
-public MyConfig config;
-public GUISize guiSize;
-public Set<String> ids;
-public String market;
+    public MyConfig config;
+    public GUISize guiSize;
+    public Set<String> ids;
+    public String market;
+    public boolean wantMaps;
 }
--- common/service/src/main/java/roart/service/ServiceResult.java
+++ common/service/src/main/java/roart/service/ServiceResult.java
@@ -7,9 +7,10 @@ import roart.config.MyConfig;
 import roart.model.ResultItem;
 
 public class ServiceResult {
-public MyConfig config;
-public List<String> markets;
-public Map<String, String> stocks;
-public List<ResultItem> list;
-public String error;
+    public MyConfig config;
+    public List<String> markets;
+    public Map<String, String> stocks;
+    public List<ResultItem> list;
+    public Map<String, Map<String, Object>> maps;
+    public String error;
 }
