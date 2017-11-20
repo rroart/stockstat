d95afd062dd3eb17846e2e01540e0819b925a0b4
Roar Thronæs <roart@nvg.ntnu.no>
2017-11-20 19:45:23 +0100

    Do save setting.

--- iclij/iclij-core/src/main/java/roart/controller/ServiceController.java
+++ iclij/iclij-core/src/main/java/roart/controller/ServiceController.java
@@ -21,9 +21,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import roart.action.FindProfitAction;
+import roart.action.ImproveProfitAction;
+import roart.action.MainAction;
 import roart.config.ConfigConstants;
 import roart.pipeline.PipelineConstants;
 import roart.service.ControlService;
+import roart.service.IclijServiceResult;
 import roart.util.Constants;
 import roart.util.ServiceUtil;
 
@@ -43,32 +47,55 @@ public class ServiceController {
         return instance;
     }
 
+    @RequestMapping(value = "/getcontent",
+            method = RequestMethod.POST)
+    public IclijServiceResult getContent(/*@PathVariable String market*/)
+            throws Exception {
+        return ServiceUtil.getContent();
+    }
+
     @RequestMapping(value = "/recommender/{market}",
             method = RequestMethod.GET)
     public void getRecommender(@PathVariable String market)
             throws Exception {
-        ServiceUtil.doRecommender(market, 0, null);
+        ServiceUtil.doRecommender(market, 0, null, true);
     }
 
     @RequestMapping(value = "/predictor/{market}",
             method = RequestMethod.GET)
     public void getPredict(@PathVariable String market)
             throws Exception {
-        ServiceUtil.doPredict(market, 0, null);
+        ServiceUtil.doPredict(market, 0, null, true);
     }
 
     @RequestMapping(value = "/mlmacd/{market}",
             method = RequestMethod.GET)
     public void getMLMACD(@PathVariable String market)
             throws Exception {
-        ServiceUtil.doMLMACD(market, 0, null);
+        ServiceUtil.doMLMACD(market, 0, null, true);
     }
 
     @RequestMapping(value = "/mlindicator/{market}",
             method = RequestMethod.GET)
     public void getMLIndicator(@PathVariable String market)
             throws Exception {
-        ServiceUtil.doMLIndicator(market, 0, null);
+        ServiceUtil.doMLIndicator(market, 0, null, true);
+    }
+
+    @RequestMapping(value = "/findprofit",
+            method = RequestMethod.GET)
+    public void getFindProfit()
+            throws Exception {
+        //MainAction.goals.add(new FindProfitAction());
+        new FindProfitAction().goal();
+    }
+
+    @RequestMapping(value = "/improveprofit",
+            method = RequestMethod.GET)
+    public void getImproveProfit()
+            throws Exception {
+        //MainAction.goals.add(new ImproveProfitAction());
+        new ImproveProfitAction().goal();
     }
 
 }
