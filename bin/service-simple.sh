4920e1133fd06326d0ff10153c56af0e6e20e184
Roar Thronæs <roart@nvg.ntnu.no>
2017-12-07 16:42:39 +0100

    Add a couple to the all-starter service.

--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -5,6 +5,8 @@ cd ~/src/stockstatspark/distribution/target/stockstat-distribution-0.5-SNAPSHOT-
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18000,suspend=n -jar stockstat-core-0.5-SNAPSHOT.jar" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18001,suspend=n -jar stockstat-iclij-core-0.5-SNAPSHOT.jar" &
+xterm -e "java -jar jetty-runner-9.4.2.v20170220.jar --port 8082 stockstat-iclij-web-0.5-SNAPSHOT.war" &
+xterm -e "java -jar jetty-runner-9.4.2.v20170220.jar stockstat-web-0.5-SNAPSHOT.war" &
 
 cd ../tensorflow
 xterm -e ./run-tf.sh &
