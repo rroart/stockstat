6f44d918569bb4c11b65eeec853cf097a6b1b987
Roar Thronæs <roart@nvg.ntnu.no>
2017-12-10 20:47:32 +0100

    Relative path fix.

--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/src/stockstatspark/distribution/target/stockstat-distribution-0.5-SNAPSHOT-bin/stockstat-distribution-0.5-SNAPSHOT/lib
+cd ../lib
 
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18000,suspend=n -jar stockstat-core-0.5-SNAPSHOT.jar" &
