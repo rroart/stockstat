136c80ccff3e6004aca16bdc2360e05a77a25726
Roar Thronæs <roart@nvg.ntnu.no>
2018-05-01 22:00:59 +0200

    Upgrade jetty.

--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -5,8 +5,8 @@ cd ../lib
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar" &
 xterm -e "java -jar stockstat-core-0.5-SNAPSHOT.jar" &
 xterm -e "java -jar stockstat-iclij-core-0.5-SNAPSHOT.jar" &
-xterm -e "java -jar jetty-runner-9.4.2.v20170220.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war" &
-xterm -e "java -jar jetty-runner-9.4.2.v20170220.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war" &
+xterm -e "java -jar jetty-runner-9.4.10.RC1.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war" &
+xterm -e "java -jar jetty-runner-9.4.10.RC1.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war" &
 
 cd ../tensorflow
 xterm -e ./flasktf.sh &
