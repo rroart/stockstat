9e3a45bfa1fcd480b1dd3c3bcedb4a71d22d5cbf
Roar Thronæs <roart@nvg.ntnu.no>
2018-05-01 22:00:11 +0200

    Update Spring Boot.

--- eureka/src/main/java/roart/EurekaApplication.java
+++ eureka/src/main/java/roart/EurekaApplication.java
@@ -2,6 +2,7 @@ package roart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) throws Exception {
-	SpringApplication.run(EurekaApplication.class, args);
+	//SpringApplication.run(EurekaApplication.class, args);
+	new SpringApplicationBuilder(EurekaApplication.class).web(true).run(args);
     }
 }
