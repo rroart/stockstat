ceda9441151035776bf3ef68f1a638db852c137b
Roar Thronæs <roart@nvg.ntnu.no>
2018-08-18 10:26:09 +0200

    Last changes for importer formatting.

--- python/pd/generatexml.py
+++ python/pd/generatexml.py
@@ -2,11 +2,12 @@ import datareader as dr
 
 import xml.etree.ElementTree as ET
 
+from datetime import datetime
+
 marketidname = 'nasdaq'
 
 reader = dr.Datareader()
 lists = reader.my(2, 90)
-
 market = ET.Element('market')
 rows = ET.SubElement(market, 'rows')
 for list in lists:
@@ -19,8 +20,11 @@ for list in lists:
         marketid = ET.SubElement(row, 'marketid')
         marketid.text = marketidname
         date = ET.SubElement(row, 'date')
-        date.text = framerow[0]
-        #.strftime('%Y-%M-%d')
+        datestr = framerow[0]
+        mydate = datetime.strptime(datestr, '%Y-%M-%d')
+        #print(framerow[0])
+        date.text = mydate.strftime('%d.%M.%Y')
+        #print(date.text)
         #date.text = str(framerow[5])
         name = ET.SubElement(row, 'id')
         name.text = ticker
@@ -28,16 +32,24 @@ for list in lists:
         name.text = myname
         price = ET.SubElement(row, 'price')
         price.text = str(framerow[4])
+        if price.text == 'nan':
+            price.text = '-'
         pricelow = ET.SubElement(row, 'pricelow')
         pricelow.text = str(framerow[3])
+        if pricelow.text == 'nan':
+            pricelow.text = '-'
         pricehigh = ET.SubElement(row, 'pricehigh')
         pricehigh.text = str(framerow[2])
-        volume= ET.SubElement(row, 'volume')
+        if pricehigh.text == 'nan':
+            pricehigh.text = '-'
+        volume = ET.SubElement(row, 'volume')
         volume.text = str(framerow[5])
+        if volume.text == 'nan':
+            volume.text = '-'
         
-ET.dump(market)
+#ET.dump(market)
 tree =  ET.ElementTree(market)
-tree.write('bla.xml')
+tree.write('/tmp/nasdaq.xml')
     
 #class GenerateXML:
 
