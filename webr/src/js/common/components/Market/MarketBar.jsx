58cd984960bcb5e9f737b5b41400e74242d83ab3
Roar Thronæs <roart@nvg.ntnu.no>
2018-10-19 22:52:18 +0200

    Datepicker. Remove state, use Redux.

--- webr/src/js/common/components/Market/MarketBar.jsx
+++ webr/src/js/common/components/Market/MarketBar.jsx
@@ -4,44 +4,53 @@ import { Client, ConvertToSelect } from '../util'
 import Select from 'react-select';
 import { DropdownButton, MenuItem, ButtonToolbar, Nav, Navbar, NavItem, FormControl } from 'react-bootstrap';
 import { ServiceParam, ServiceResult } from '../../types/main'
+import DatePicker from 'react-16-bootstrap-date-picker';
 
 class MarketBar extends PureComponent {
   type : string;
-  state = {
-    markets: ServiceResult,
-  }
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    console.log("here");
+    console.log(props);
+    console.log(this.props);
     var serviceparam = new ServiceParam();
     serviceparam.market = '0';
     Client.search("/getmarkets", serviceparam, (markets) => {
-      this.setState({
-        markets: markets
-      });
+    console.log("here");
+      console.log(markets);
+      console.log(this.props);
+      this.props.setmarkets(markets.markets);
     });
-    console.log(this.state.markets);
+      var value = new Date().toISOString();
+      console.log(this.props);
+      this.props.setenddate(value);
+    console.log("here");
+    //console.log(this.state.markets);
 }
 
 handleYearChange = (e) => {
   console.log(e);
+  console.log(this);
   const value = e.value;
   var result;
-  var serviceparam = new ServiceParam();
-    Client.search("/getmarkets", serviceparam, (markets) => {
-    this.setState({
-      markets: markets
-    });
-  });
 }
 
-    handleChange(event) {
+    handleChange(event, props) {
     console.log(event);
     console.log(event.value);
+    console.log(props);
+    //this.props.setmarket({market: event.value});
+    props.setmarket(event.value);
   }
   
   render() {
-  //console.log(this.state.markets);
-  var markets2 = ConvertToSelect.convert(this.state.markets.markets);
+    const { main } = this.props;
+    console.log(main);
+    const markets = main && main.markets ? main.markets : null;
+    const startdate = main && main.startdate ? main.startdate : null;
+    const enddate = main && main.enddate ? main.enddate : null;
+  console.log(markets);
+  var markets2 = ConvertToSelect.convert2(markets);
   console.log(markets2);
     return (
       <div>
@@ -55,10 +64,18 @@ handleYearChange = (e) => {
           <NavItem eventKey={1} href="#">
             Name
             <Select options="[{size:'5'}]"
-        onChange={this.handleChange}
+        onChange={e => this.handleChange(e, this.props)}
         options={markets2}
       />
     </NavItem>
+          <NavItem eventKey={2} href="#">
+            Start date
+	    <DatePicker id="startdatepicker" value={startdate} onChange={this.handleStartDateChange}/>
+	    </NavItem>
+          <NavItem eventKey={3} href="#">
+            End date
+	    <DatePicker id="enddatepicker" value={enddate} onChange={this.handleEndDateChange}/>
+	    </NavItem>
             </Nav>
           </Navbar>
       </div>
