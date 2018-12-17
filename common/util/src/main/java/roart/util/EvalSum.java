88bff6b58c79a94e845841648f1fb538d079fd17
Roar Thronæs <roart@nvg.ntnu.no>
2018-12-17 12:57:21 +0100

    Temp for for dead class.

--- common/util/src/main/java/roart/util/EvalSum.java
+++ common/util/src/main/java/roart/util/EvalSum.java
@@ -5,13 +5,28 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class EvalSum extends EvalUtil {
-    public double calculate(Map<String, List<Double>> list) {
+    public double calculateResult(Map<String, List<Double>> resultMap) {
+        double result = 0;
+        double[] array = calculate(resultMap);
+        result += array[0] / array[1];
+        return result;
+    }
+    
+    public double[] calculate(Map<String, List<Double>> list) {
         double result = 0;
         for (Entry<String, List<Double>> entry : list.entrySet()) {
             List<Double> resultList = entry.getValue();
             result += resultList.get(0);
         }
-        return result;
+        double[] array = new double[2];
+        array[0] = result;
+        array[1] = result;
+        return array;
+    }
+
+    @Override
+    public String name() {
+        return "sum";
     }
 
 }
