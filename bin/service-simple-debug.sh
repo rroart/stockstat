03ac807d9d62670f8b7f33a714090ecf3e7173dd
Roar Thronæs <roart@nvg.ntnu.no>
2018-12-23 18:43:29 +0100

    Upgrade jetty. Upgrade to Spring Boot 2. Use version vars in the poms. Tidy poms.

--- bin/service-simple-debug.sh
+++ bin/service-simple-debug.sh
@@ -5,8 +5,8 @@ cd ../lib
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/eureka.out" &
 xterm -e "/opt/jdk8/bin/java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18000,suspend=n -jar stockstat-core-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/core.out" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18001,suspend=n -jar stockstat-iclij-core-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/iclij.out " &
-xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18002,suspend=n -jar jetty-runner-9.4.10.RC1.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/iclijweb.out" &
-xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18003,suspend=n -jar jetty-runner-9.4.10.RC1.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/web.out" &
+xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18002,suspend=n -jar jetty-runner-9.4.12.v20180830.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/iclijweb.out" &
+xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18003,suspend=n -jar jetty-runner-9.4.12.v20180830.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/web.out" &
 
 cd ../tensorflow
 xterm -e "./flasktf.sh 2>&1 | tee /tmp/flask.out" &
--- bin/service-simple.sh
+++ bin/service-simple.sh
@@ -5,8 +5,8 @@ cd ../lib
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/eureka.out" &
 xterm -e "java -jar stockstat-core-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/core.out" &
 xterm -e "java -jar stockstat-iclij-core-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/iclij.out" &
-xterm -e "java -jar jetty-runner-9.4.10.RC1.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/iclijweb.out" &
-xterm -e "java -jar jetty-runner-9.4.10.RC1.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/web.out" &
+xterm -e "java -jar jetty-runner-9.4.12.v20180830.jar --port 8181 stockstat-iclij-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/iclijweb.out" &
+xterm -e "java -jar jetty-runner-9.4.12.v20180830.jar --port 8180 stockstat-web-0.5-SNAPSHOT.war 2>&1 | tee /tmp/web.out" &
 
 cd ../tensorflow
 xterm -e "./flasktf.sh 2>&1 | tee /tmp/flask.out" &
