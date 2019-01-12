70bcc7509a215f84fceaf3ffc7f07857888322df
Roar Thronæs <roart@nvg.ntnu.no>
2019-01-12 10:05:19 +0100

    Remove jdk8.

--- bin/docboevol.sh
+++ bin/docboevol.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DIRNAME=`dirname $0`
 $DIRNAME/cboevol.sh
-JAVA_HOME=/opt/jdk8 java -jar $DIRNAME/../input/target/stockstat-input-0.5-SNAPSHOT-jar-with-dependencies.jar /tmp/cboevol.xml
+java -jar $DIRNAME/../input/target/stockstat-input-0.5-SNAPSHOT-jar-with-dependencies.jar /tmp/cboevol.xml
