5d391e425bea4f7421d1647bd0893bb3944477ef
Roar Thronæs <roart@nvg.ntnu.no>
2019-01-12 09:29:02 +0100

    Add missing directory.

---
+++ main/ml/common/src/main/java/roart/ml/model/LearnTestClassify.java
@@ -0,0 +1,152 @@
+package roart.ml.model;
+
+import roart.common.ml.TensorflowDNNConfig;
+import roart.common.ml.TensorflowDNNLConfig;
+import roart.common.ml.TensorflowLConfig;
+
+public class LearnTestClassify {
+    private TensorflowDNNConfig tensorflowDNNConfig;
+    
+    private TensorflowDNNLConfig tensorflowDNNLConfig;
+    
+    private TensorflowLConfig tensorflowLConfig;
+    
+    private Object[][] trainingarray;
+
+    private Object[] trainingcatarray;
+    
+    private Object[][] classifyarray;
+
+    private Object[] classifycatarray;
+
+    private Object[] classifyprobarray;
+
+    private int modelInt;
+    
+    private int size;
+    
+    private String period;
+    
+    private String mapname;
+    
+    private int outcomes;
+    
+    private Double accuracy;
+
+    public TensorflowDNNConfig getTensorflowDNNConfig() {
+        return tensorflowDNNConfig;
+    }
+
+    public void setTensorflowDNNConfig(TensorflowDNNConfig tensorflowDNNConfig) {
+        this.tensorflowDNNConfig = tensorflowDNNConfig;
+    }
+
+    public TensorflowDNNLConfig getTensorflowDNNLConfig() {
+        return tensorflowDNNLConfig;
+    }
+
+    public void setTensorflowDNNLConfig(TensorflowDNNLConfig tensorflowDNNLConfig) {
+        this.tensorflowDNNLConfig = tensorflowDNNLConfig;
+    }
+
+    public TensorflowLConfig getTensorflowLConfig() {
+        return tensorflowLConfig;
+    }
+
+    public void setTensorflowLConfig(TensorflowLConfig tensorflowLConfig) {
+        this.tensorflowLConfig = tensorflowLConfig;
+    }
+
+    public LearnTestClassify() {
+        super();
+    }
+
+    public Object[][] getTrainingarray() {
+        return trainingarray;
+    }
+
+    public void setTrainingarray(Object[][] trainingarray) {
+        this.trainingarray = trainingarray;
+    }
+
+    public Object[] getTrainingcatarray() {
+        return trainingcatarray;
+    }
+
+    public void setTrainingcatarray(Object[] catarray) {
+        this.trainingcatarray = catarray;
+    }
+
+    public Object[][] getClassifyarray() {
+        return classifyarray;
+    }
+
+    public void setClassifyarray(Object[][] classifyarray) {
+        this.classifyarray = classifyarray;
+    }
+
+    public Object[] getClassifycatarray() {
+        return classifycatarray;
+    }
+
+    public void setClassifycatarray(Object[] classifycatarray) {
+        this.classifycatarray = classifycatarray;
+    }
+
+    public Object[] getClassifyprobarray() {
+        return classifyprobarray;
+    }
+
+    public void setClassifyprobarray(Object[] classifyprobarray) {
+        this.classifyprobarray = classifyprobarray;
+    }
+
+    public int getModelInt() {
+        return modelInt;
+    }
+
+    public void setModelInt(int modelInt) {
+        this.modelInt = modelInt;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public String getMapname() {
+        return mapname;
+    }
+
+    public void setMapname(String mapname) {
+        this.mapname = mapname;
+    }
+
+    public int getOutcomes() {
+        return outcomes;
+    }
+
+    public void setOutcomes(int outcomes) {
+        this.outcomes = outcomes;
+    }
+
+    public Double getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(Double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+}
---
+++ main/ml/common/src/main/java/roart/ml/model/LearnTestPredict.java
@@ -0,0 +1,24 @@
+package roart.ml.model;
+
+import java.util.List;
+
+public class LearnTestPredict {
+    public List<Double[]> slides;
+    //public List<Double> next;
+    public Object[] array;
+    public List<Object[]> arraylist;
+    public Object[] cat;
+    public List<List<Object>> listlist;
+    public int modelInt;
+    public int size;
+    public String period;
+    public String mapname;
+    public int outcomes;
+    public Double prob;
+    public int horizon;
+    public int epochs;
+    public int windowsize;
+    public Double[] predicted;
+    public List<Double[]> predictedlist;
+    public Double accuracy;
+}
