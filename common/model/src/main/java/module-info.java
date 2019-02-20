8cfbc56edb6a9a8c0f82db811a198103722036be
Roar Thronæs <roart@nvg.ntnu.no>
2019-02-20 12:25:35 +0100

    Add a new top level abstract class for results for all indicators etc.

--- common/model/src/main/java/module-info.java
+++ common/model/src/main/java/module-info.java
@@ -6,6 +6,7 @@
  *
  */
 module common.model {
+    exports roart.common.pipeline.model;
     exports roart.result.model;
     requires jackson.annotations;
 
---
+++ common/model/src/main/java/roart/common/pipeline/model/PipelineResultData.java
@@ -0,0 +1,9 @@
+package roart.common.pipeline.model;
+
+import java.util.Map;
+
+public abstract class PipelineResultData {
+
+    public abstract Map<String, Object> getLocalResultMap();
+
+}
