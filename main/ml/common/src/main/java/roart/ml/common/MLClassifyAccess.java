781d2df47ae126f49a37ca294204ee2f6bc972ab
Roar Thronæs <roart@nvg.ntnu.no>
2019-02-20 12:31:45 +0100

    Renamed classes.

--- main/ml/common/src/main/java/roart/ml/common/MLClassifyAccess.java
+++ main/ml/common/src/main/java/roart/ml/common/MLClassifyAccess.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import roart.common.ml.NNConfigs;
+import roart.common.ml.NeuralNetConfigs;
 import roart.ml.model.LearnTestClassifyResult;
 import roart.pipeline.common.aggregate.Aggregator;
 
@@ -16,13 +16,13 @@ public abstract class MLClassifyAccess {
 
     protected List<MLClassifyModel> models;
     
-    public abstract Double learntest(NNConfigs nnconfigs, Aggregator indicator, Map<double[], Double> map, MLClassifyModel model, int size, String period, String mapname, int outcomes);
+    public abstract Double learntest(NeuralNetConfigs nnconfigs, Aggregator indicator, Map<double[], Double> map, MLClassifyModel model, int size, String period, String mapname, int outcomes);
 
     public abstract Double eval(int modelInt, String period, String mapname);
 
     public abstract Map<String, Double[]> classify(Aggregator indicator, Map<String, double[]> map, MLClassifyModel model, int size, String period, String mapname, int outcomes, Map<Double, String> shortMap);
 
-    public abstract LearnTestClassifyResult learntestclassify(NNConfigs nnconfigs, Aggregator indicator, Map<double[], Double> map, MLClassifyModel model, int size, String period, String mapname, int outcomes, Map<String, double[]> map2, Map<Double, String> shortMap);
+    public abstract LearnTestClassifyResult learntestclassify(NeuralNetConfigs nnconfigs, Aggregator indicator, Map<double[], Double> map, MLClassifyModel model, int size, String period, String mapname, int outcomes, Map<String, double[]> map2, Map<Double, String> shortMap);
 
     public abstract List<MLClassifyModel> getModels();
 
