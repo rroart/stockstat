439fac21dbe5e6e4eea2fad8a78fdc20162096f2
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-12 09:07:37 +0100

    Add support for another db (I13).

--- bin/service-simple-dev.sh
+++ bin/service-simple-dev.sh
@@ -5,11 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DEV="--spring.profiles.active=dev"
 
+DB="-Dconnection.url=jdbc:postgresql://localhost:5432/stockstatdev"
+
 cd ../lib
 
 xterm -e "java -jar stockstat-eureka-0.5-SNAPSHOT.jar 2>&1 | tee /tmp/eurekadev.out" &
-xterm -e "$JAVA10 -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18000,suspend=n -jar stockstat-core-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/coredev.out" &
-xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18001,suspend=n -jar stockstat-iclij-core-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/iclijdev.out " &
+xterm -e "$JAVA10 $DB -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18000,suspend=n -jar stockstat-core-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/coredev.out" &
+xterm -e "java $DB -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18001,suspend=n -jar stockstat-iclij-core-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/iclijdev.out " &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18002,suspend=n -jar stockstat-iclij-web-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/iclijwebdev.out" &
 xterm -e "java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=18003,suspend=n -jar stockstat-web-0.5-SNAPSHOT.jar $DEV 2>&1 | tee /tmp/webdev.out" &
 
