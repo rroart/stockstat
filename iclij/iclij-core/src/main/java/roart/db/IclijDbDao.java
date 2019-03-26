25a3bd556a63a653d90c868e4aff512a094bae23
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-26 23:02:30 +0100

    Run new first, then heaviest (I27). Restart interrupted findprofit (I23). Save ML config, updatemap (I11).

--- iclij/iclij-core/src/main/java/roart/db/IclijDbDao.java
+++ iclij/iclij-core/src/main/java/roart/db/IclijDbDao.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import roart.iclij.model.MemoryItem;
+import roart.iclij.model.TimingItem;
 
 public class IclijDbDao {
 	private static Logger log = LoggerFactory.getLogger(IclijDbDao.class);
@@ -17,5 +18,10 @@ public class IclijDbDao {
 	public static List<MemoryItem> getAll(String type) throws Exception {
 		return MemoryItem.getAll(type);
 	}
+
+	public static List<TimingItem> getAllTiming() throws Exception {
+	    return TimingItem.getAll();        
+	}
+
 }
 
