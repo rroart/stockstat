d1f826139c0ea60f0a7e062eb8054a6ae979c442
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-26 22:56:04 +0100

    Save ML config, updatemap (I11).

---
+++ common/db/src/main/java/roart/db/model/Config.java
@@ -0,0 +1,145 @@
+package roart.db.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "Config")
+@org.hibernate.annotations.Table(appliesTo = "Config")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Config implements Serializable {
+
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Id
+    @Column
+    private Long dbid;
+
+    @Column
+    private String market;
+
+    @Column
+    private String action;
+
+    @Column
+    private String component;
+
+    @Column
+    private String id;
+
+    @Column
+    private String value;
+
+    @Column
+    private Double score;
+
+    @Column
+    private Date record;
+
+    @Column
+    private Date date;
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public Date getRecord() {
+        return record;
+    }
+
+    public void setRecord(Date record) {
+        this.record = record;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Transient
+    public static List<Config> getAll() throws Exception {
+        Session session = HibernateUtil.getMyHibernateSession();
+        Transaction transaction = session.beginTransaction();
+        List<Config> list = session.createQuery("from Config").list();
+        transaction.commit();
+        return list;
+    }
+
+    @Transient
+    public static List<Config> getAll(String mymarket) throws Exception {
+        Session session = HibernateUtil.getMyHibernateSession();
+        Transaction transaction = session.beginTransaction();
+        List<Config> list = session.createQuery("from Config where market = :mymarket").setParameter("mymarket",  mymarket).list();
+        transaction.commit();
+        return list;
+    }
+
+    @Transient
+    public void save() throws Exception {
+        Session session = HibernateUtil.getMyHibernateSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(this);
+        transaction.commit();
+    }
+
+}
---
+++ iclij/iclij-common/iclij-model/src/main/java/roart/iclij/model/ConfigItem.java
@@ -0,0 +1,141 @@
+package roart.iclij.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import roart.common.util.TimeUtil;
+import roart.db.model.Config;
+
+public class ConfigItem {
+    private LocalDate record;
+    
+    private LocalDate date;
+    
+    private String market;
+
+    private String component;
+
+    private String action;
+    
+    private String id;
+    
+    private String value;
+    
+    private Double score;
+
+    public LocalDate getRecord() {
+        return record;
+    }
+
+    public void setRecord(LocalDate record) {
+        this.record = record;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return market + " " + component + " " + action + " " + record + " " + date + " " + id + " " + value + " " + score + "\n"; 
+    }
+    
+    public void save() throws Exception {
+        Config config = new Config();
+        config.setAction(getAction());
+        config.setComponent(getComponent());
+        config.setDate(TimeUtil.convertDate(getDate()));
+        config.setId(getId());
+        config.setMarket(getMarket());
+        config.setRecord(TimeUtil.convertDate(getRecord()));
+        config.setScore(getScore());
+        config.setValue(getValue());
+        config.save();
+    }
+    
+    public static List<ConfigItem> getAll() throws Exception {
+        List<Config> configs = Config.getAll();
+        List<ConfigItem> configItems = new ArrayList<>();
+        for (Config config : configs) {
+            ConfigItem memoryItem = getConfigItem(config);
+            configItems.add(memoryItem);
+        }
+        return configItems;
+    }
+
+   public static List<ConfigItem> getAll(String market) throws Exception {
+        List<Config> configs = Config.getAll(market);
+        List<ConfigItem> configItems = new ArrayList<>();
+        for (Config config : configs) {
+            ConfigItem memoryItem = getConfigItem(config);
+            configItems.add(memoryItem);
+        }
+        return configItems;
+    }
+
+    private static ConfigItem getConfigItem(Config config) {
+        ConfigItem configItem = new ConfigItem();
+        configItem.setDate(TimeUtil.convertDate(config.getDate()));
+        configItem.setId(config.getId());
+        configItem.setComponent(config.getComponent());
+        configItem.setMarket(config.getMarket());
+        configItem.setRecord(TimeUtil.convertDate(config.getRecord()));
+        configItem.setScore(config.getScore());
+        configItem.setValue(config.getValue());
+        return configItem;
+    }
+
+}
