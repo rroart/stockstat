9fa307bb81a0dca14011c925bbd0853d98782520
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-29 08:48:16 +0100

    New constants for refactoring find/improveprofit (I14).

--- iclij/iclij-common/iclij-constants/src/main/java/roart/constants/IclijConstants.java
+++ iclij/iclij-common/iclij-constants/src/main/java/roart/constants/IclijConstants.java
@@ -1,5 +1,6 @@
 package roart.constants;
 
 public class IclijConstants {
-
+    public final static String FINDPROFIT = "findprofit";
+    public final static String IMPROVEPROFIT = "improveprofit";
 }
