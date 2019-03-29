a4e2e8a75cf15b344b028678864887183e05b337
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-29 09:03:41 +0100

    Indentation.

--- iclij/iclij-core/src/main/java/roart/db/IclijDbDao.java
+++ iclij/iclij-core/src/main/java/roart/db/IclijDbDao.java
@@ -9,19 +9,19 @@ import roart.iclij.model.MemoryItem;
 import roart.iclij.model.TimingItem;
 
 public class IclijDbDao {
-	private static Logger log = LoggerFactory.getLogger(IclijDbDao.class);
+    private static Logger log = LoggerFactory.getLogger(IclijDbDao.class);
 
     public static List<MemoryItem> getAll() throws Exception {
         return MemoryItem.getAll();
     }
 
-	public static List<MemoryItem> getAll(String type) throws Exception {
-		return MemoryItem.getAll(type);
-	}
+    public static List<MemoryItem> getAll(String type) throws Exception {
+        return MemoryItem.getAll(type);
+    }
 
-	public static List<TimingItem> getAllTiming() throws Exception {
-	    return TimingItem.getAll();        
-	}
+    public static List<TimingItem> getAllTiming() throws Exception {
+        return TimingItem.getAll();        
+    }
 
 }
 
