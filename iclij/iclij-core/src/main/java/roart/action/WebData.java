f39fd5fdd49478aaaf00dd1eaeab89a34f742944
Roar Thronæs <roart@nvg.ntnu.no>
2019-03-29 08:42:01 +0100

    New class for refactoring findprofit (I14).

---
+++ iclij/iclij-core/src/main/java/roart/action/WebData.java
@@ -0,0 +1,13 @@
+package roart.action;
+
+import java.util.List;
+import java.util.Map;
+
+import roart.iclij.model.MemoryItem;
+import roart.service.model.ProfitData;
+
+public class WebData {
+    public ProfitData profitData;
+    public List<MemoryItem> memoryItems;
+    public Map<String, Object> updateMap;
+}
