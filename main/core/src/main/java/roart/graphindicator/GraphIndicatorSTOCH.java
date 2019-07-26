9ee7c3e7519ab4ad52dafbd71416ce6678d4c7f7
Roar Thronæs <roart@nvg.ntnu.no>
2019-07-26 22:29:13 +0200

    Add new TA lib (I67).

--- main/core/src/main/java/roart/graphindicator/GraphIndicatorSTOCH.java
+++ main/core/src/main/java/roart/graphindicator/GraphIndicatorSTOCH.java
@@ -1,11 +1,19 @@
 package roart.graphindicator;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang3.tuple.Pair;
 
 import roart.common.config.MyMyConfig;
 import roart.model.data.MarketData;
 import roart.model.data.PeriodData;
+import roart.result.model.GUISize;
+import roart.result.model.ResultItem;
 import roart.talib.Ta;
+import roart.talib.impl.Ta4jSTOCH;
+import roart.talib.impl.Ta4jSTOCHRSI;
 import roart.talib.impl.TalibSTOCH;
 
 public class GraphIndicatorSTOCH extends GraphIndicator {
@@ -17,6 +25,12 @@ public class GraphIndicatorSTOCH extends GraphIndicator {
     @Override
     public boolean isEnabled() {
         return conf.isSTOCHEnabled();
+    }
+
+    @Override
+    public void getResult(List<ResultItem> retlist, Set<Pair<String, String>> ids, GUISize guiSize) {
+        super.getResult(retlist, ids, guiSize);
+        super.getResult(retlist, ids, guiSize, new Ta4jSTOCH());
     }
 
     @Override
