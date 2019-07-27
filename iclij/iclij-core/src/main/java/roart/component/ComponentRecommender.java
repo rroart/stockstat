dda1d54d942b8a35a40c54e485c1a392f973bab7
Roar Thronæs <roart@nvg.ntnu.no>
2019-07-27 22:42:12 +0200

    Remove duplicates, related to disabling all components first (I94).

--- iclij/iclij-core/src/main/java/roart/component/ComponentRecommender.java
+++ iclij/iclij-core/src/main/java/roart/component/ComponentRecommender.java
@@ -76,7 +76,6 @@ public class ComponentRecommender extends ComponentNoML {
         valueMap.put(ConfigConstants.INDICATORSMACDMACDHISTOGRAMDELTA, Boolean.TRUE);                
         valueMap.put(ConfigConstants.INDICATORSMACDMACDMACDDELTA, Boolean.TRUE);                
         valueMap.put(ConfigConstants.INDICATORSMACDMACDSIGNALDELTA, Boolean.TRUE);                
-        valueMap.put(ConfigConstants.INDICATORSRSI, Boolean.TRUE);                
         valueMap.put(ConfigConstants.INDICATORSSTOCHSTOCHDELTA, Boolean.TRUE);                
         valueMap.put(ConfigConstants.INDICATORSSTOCHRSIDELTA, Boolean.TRUE);                
         valueMap.put(ConfigConstants.INDICATORSRSIRECOMMEND, Boolean.TRUE);
@@ -105,7 +104,6 @@ public class ComponentRecommender extends ComponentNoML {
         valueMap.put(ConfigConstants.INDICATORSMACDMACDHISTOGRAMDELTA, Boolean.FALSE);                
         valueMap.put(ConfigConstants.INDICATORSMACDMACDMACDDELTA, Boolean.FALSE);                
         valueMap.put(ConfigConstants.INDICATORSMACDMACDSIGNALDELTA, Boolean.FALSE);                
-        valueMap.put(ConfigConstants.INDICATORSRSI, Boolean.FALSE);                
         valueMap.put(ConfigConstants.INDICATORSSTOCHSTOCHDELTA, Boolean.FALSE);                
         valueMap.put(ConfigConstants.INDICATORSSTOCHRSIDELTA, Boolean.FALSE);                
         valueMap.put(ConfigConstants.INDICATORSRSIRECOMMEND, Boolean.FALSE);
