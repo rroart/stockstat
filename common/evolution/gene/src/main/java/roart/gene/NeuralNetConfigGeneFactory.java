2d11d86b2474b4d9433b17023a4787e80223e9cf
Roar Thronæs <roart@nvg.ntnu.no>
2019-09-13 13:17:02 +0200

    Add pytorch (I99). Neural net continual learning (I100). Tensorflow extend and refactor (I101). Spark renames (I112).

---
+++ common/evolution/gene/src/main/java/roart/gene/NeuralNetConfigGeneFactory.java
@@ -0,0 +1,205 @@
+package roart.gene;
+
+import roart.common.config.ConfigConstants;
+import roart.common.ml.GemEWCConfig;
+import roart.common.ml.GemGEMConfig;
+import roart.common.ml.GemIConfig;
+import roart.common.ml.GemIcarlConfig;
+import roart.common.ml.GemMMConfig;
+import roart.common.ml.GemSConfig;
+import roart.common.ml.NeuralNetConfig;
+import roart.common.ml.NeuralNetConfigs;
+import roart.common.ml.PytorchCNNConfig;
+import roart.common.ml.PytorchGRUConfig;
+import roart.common.ml.PytorchLSTMConfig;
+import roart.common.ml.PytorchMLPConfig;
+import roart.common.ml.PytorchRNNConfig;
+import roart.common.ml.SparkLORConfig;
+import roart.common.ml.SparkLSVCConfig;
+import roart.common.ml.SparkMLPCConfig;
+import roart.common.ml.SparkOVRConfig;
+import roart.common.ml.TensorflowCNNConfig;
+import roart.common.ml.TensorflowDNNConfig;
+import roart.common.ml.TensorflowGRUConfig;
+import roart.common.ml.TensorflowLICConfig;
+import roart.common.ml.TensorflowLIRConfig;
+import roart.common.ml.TensorflowLSTMConfig;
+import roart.common.ml.TensorflowMLPConfig;
+import roart.common.ml.TensorflowPredictorLSTMConfig;
+import roart.common.ml.TensorflowRNNConfig;
+import roart.gene.ml.impl.GemEWCConfigGene;
+import roart.gene.ml.impl.GemGEMConfigGene;
+import roart.gene.ml.impl.GemIConfigGene;
+import roart.gene.ml.impl.GemIcarlConfigGene;
+import roart.gene.ml.impl.GemMMConfigGene;
+import roart.gene.ml.impl.GemSConfigGene;
+import roart.gene.ml.impl.PytorchCNNConfigGene;
+import roart.gene.ml.impl.PytorchGRUConfigGene;
+import roart.gene.ml.impl.PytorchLSTMConfigGene;
+import roart.gene.ml.impl.PytorchMLPConfigGene;
+import roart.gene.ml.impl.PytorchRNNConfigGene;
+import roart.gene.ml.impl.SparkLORConfigGene;
+import roart.gene.ml.impl.SparkLSVCConfigGene;
+import roart.gene.ml.impl.SparkMLPCConfigGene;
+import roart.gene.ml.impl.SparkOVRConfigGene;
+import roart.gene.ml.impl.TensorflowCNNConfigGene;
+import roart.gene.ml.impl.TensorflowDNNConfigGene;
+import roart.gene.ml.impl.TensorflowGRUConfigGene;
+import roart.gene.ml.impl.TensorflowLICConfigGene;
+import roart.gene.ml.impl.TensorflowLIRConfigGene;
+import roart.gene.ml.impl.TensorflowLSTMConfigGene;
+import roart.gene.ml.impl.TensorflowMLPConfigGene;
+import roart.gene.ml.impl.TensorflowPredictorLSTMConfigGene;
+import roart.gene.ml.impl.TensorflowRNNConfigGene;
+
+public class NeuralNetConfigGeneFactory {
+    public static NeuralNetConfigGene get(NeuralNetConfig config, String key) {
+        switch (key) {
+        case ConfigConstants.MACHINELEARNINGSPARKMLLOR:
+            return get((SparkLORConfig) config);
+        case ConfigConstants.MACHINELEARNINGSPARKMLMLPC:
+            return get((SparkMLPCConfig) config);
+        case ConfigConstants.MACHINELEARNINGSPARKMLOVR:
+            return get((SparkOVRConfig) config);
+        case ConfigConstants.MACHINELEARNINGSPARKMLLSVC:
+            return get((SparkLSVCConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWDNN:
+            return get((TensorflowDNNConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWLIC:
+            return get((TensorflowLICConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWLIR:
+            return get((TensorflowLIRConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWMLP:
+            return get((TensorflowMLPConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWCNN:
+            return get((TensorflowCNNConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWRNN:
+            return get((TensorflowRNNConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWGRU:
+            return get((TensorflowGRUConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWLSTM:
+            return get((TensorflowLSTMConfig) config);
+        case ConfigConstants.MACHINELEARNINGTENSORFLOWPREDICTORLSTM:
+            return get((TensorflowPredictorLSTMConfig) config);
+        case ConfigConstants.MACHINELEARNINGPYTORCHMLP:
+            return get((PytorchMLPConfig) config);
+        case ConfigConstants.MACHINELEARNINGPYTORCHCNN:
+            return get((PytorchCNNConfig) config);
+        case ConfigConstants.MACHINELEARNINGPYTORCHRNN:
+            return get((PytorchRNNConfig) config);
+        case ConfigConstants.MACHINELEARNINGPYTORCHGRU:
+            return get((PytorchGRUConfig) config);
+        case ConfigConstants.MACHINELEARNINGPYTORCHLSTM:
+            return get((PytorchLSTMConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMEWC:
+	    return get((GemEWCConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMGEM:
+	    return get((GemGEMConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMINDEPENDENT:
+	    return get((GemIConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMICARL:
+	    return get((GemIcarlConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMMULTIMODAL:
+	    return get((GemMMConfig) config);
+	case ConfigConstants.MACHINELEARNINGGEMSINGLE:
+	    return get((GemSConfig) config);
+        }
+        return null;
+    }
+    
+    public static NeuralNetConfigGene get(SparkMLPCConfig config) {
+        return new SparkMLPCConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(SparkLORConfig config) {
+        return new SparkLORConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(SparkOVRConfig config) {
+        return new SparkOVRConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(SparkLSVCConfig config) {
+        return new SparkLSVCConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowDNNConfig config) {
+        return new TensorflowDNNConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowLICConfig config) {
+        return new TensorflowLICConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowLIRConfig config) {
+        return new TensorflowLIRConfigGene(config);
+    }
+     
+    public static NeuralNetConfigGene get(TensorflowMLPConfig config) {
+        return new TensorflowMLPConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowCNNConfig config) {
+        return new TensorflowCNNConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowRNNConfig config) {
+        return new TensorflowRNNConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowGRUConfig config) {
+        return new TensorflowGRUConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowLSTMConfig config) {
+        return new TensorflowLSTMConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(TensorflowPredictorLSTMConfig config) {
+        return new TensorflowPredictorLSTMConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(PytorchMLPConfig config) {
+        return new PytorchMLPConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(PytorchCNNConfig config) {
+        return new PytorchCNNConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(PytorchRNNConfig config) {
+        return new PytorchRNNConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(PytorchGRUConfig config) {
+        return new PytorchGRUConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(PytorchLSTMConfig config) {
+        return new PytorchLSTMConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemEWCConfig config) {
+        return new GemEWCConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemGEMConfig config) {
+        return new GemGEMConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemIConfig config) {
+        return new GemIConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemIcarlConfig config) {
+        return new GemIcarlConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemMMConfig config) {
+        return new GemMMConfigGene(config);
+    }
+    
+    public static NeuralNetConfigGene get(GemSConfig config) {
+        return new GemSConfigGene(config);
+    }
+}
