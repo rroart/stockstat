e5b798b46628b1c11d4c7bc306ba1fc1aa2ec37f
Roar Thronæs <roart@nvg.ntnu.no>
2019-09-13 13:20:20 +0200

    Evolution refactor, chromosome to contain gene, containing config (I111).

---
+++ common/util/src/main/java/roart/common/util/RandomUtil.java
@@ -0,0 +1,27 @@
+package roart.common.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+    
+    public static int random(Random random, int min, int max) {
+        return random(random, min, max - min);
+    }
+    
+    public static int random(Random random, int base, int multiple, int range) {
+        return base + multiple * random.nextInt(range);
+    }
+    
+    public static double random(Random random, double base, double multiple, int range) {
+        return base + multiple * random.nextInt(range);
+    }
+    
+    public static double random(Random random, double base, double range) {
+        return base + range * random.nextDouble();
+    }
+    
+    public static double generatePow(Random random, double base, int min, int max) {
+        return Math.pow(base, random(random, min, max + 1));
+    }
+ 
+}
