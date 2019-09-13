23468f7e6f453c6023beef2cc4b3b11fa3d6e110
Roar Thronæs <roart@nvg.ntnu.no>
2019-09-13 11:57:40 +0200

    Add pytorch (I99). Neural net continual learning (I100). Tensorflow extend and refactor (I101). Spark renames (I112).

--- main/ml/common/src/main/java/roart/ml/model/LearnTestClassify.java
+++ main/ml/common/src/main/java/roart/ml/model/LearnTestClassify.java
@@ -1,15 +1,67 @@
 package roart.ml.model;
 
+import roart.common.ml.GemEWCConfig;
+import roart.common.ml.GemGEMConfig;
+import roart.common.ml.GemIConfig;
+import roart.common.ml.GemIcarlConfig;
+import roart.common.ml.GemMMConfig;
+import roart.common.ml.GemSConfig;
+import roart.common.ml.PytorchCNNConfig;
+import roart.common.ml.PytorchGRUConfig;
+import roart.common.ml.PytorchLSTMConfig;
+import roart.common.ml.PytorchMLPConfig;
+import roart.common.ml.PytorchRNNConfig;
+import roart.common.ml.TensorflowCNNConfig;
 import roart.common.ml.TensorflowDNNConfig;
-import roart.common.ml.TensorflowDNNLConfig;
-import roart.common.ml.TensorflowLConfig;
+import roart.common.ml.TensorflowGRUConfig;
+import roart.common.ml.TensorflowLICConfig;
+import roart.common.ml.TensorflowLIRConfig;
+import roart.common.ml.TensorflowLSTMConfig;
+import roart.common.ml.TensorflowMLPConfig;
+import roart.common.ml.TensorflowPredictorLSTMConfig;
+import roart.common.ml.TensorflowRNNConfig;
 
 public class LearnTestClassify {
+    
     private TensorflowDNNConfig tensorflowDNNConfig;
     
-    private TensorflowDNNLConfig tensorflowDNNLConfig;
+    private TensorflowLICConfig tensorflowLICConfig;
     
-    private TensorflowLConfig tensorflowLConfig;
+    private TensorflowLIRConfig tensorflowLIRConfig;
+
+    private TensorflowPredictorLSTMConfig tensorflowPredictorLSTMConfig;
+
+    private TensorflowMLPConfig tensorflowMLPConfig;
+    
+    private TensorflowCNNConfig tensorflowCNNConfig;
+    
+    private TensorflowRNNConfig tensorflowRNNConfig;
+    
+    private TensorflowLSTMConfig tensorflowLSTMConfig;
+    
+    private TensorflowGRUConfig tensorflowGRUConfig;
+    
+    private PytorchMLPConfig pytorchMLPConfig;
+    
+    private PytorchCNNConfig pytorchCNNConfig;
+    
+    private PytorchRNNConfig pytorchRNNConfig;
+    
+    private PytorchLSTMConfig pytorchLSTMConfig;
+    
+    private PytorchGRUConfig pytorchGRUConfig;
+    
+    private GemSConfig gemSConfig;
+    
+    private GemIConfig gemIConfig;
+    
+    private GemMMConfig gemMMConfig;
+    
+    private GemEWCConfig gemEWCConfig;
+    
+    private GemGEMConfig gemGEMConfig;
+    
+    private GemIcarlConfig gemIcarlConfig;
     
     private Object[][] trainingarray;
 
@@ -45,20 +97,156 @@ public class LearnTestClassify {
         this.tensorflowDNNConfig = tensorflowDNNConfig;
     }
 
-    public TensorflowDNNLConfig getTensorflowDNNLConfig() {
-        return tensorflowDNNLConfig;
+    public TensorflowLICConfig getTensorflowLICConfig() {
+        return tensorflowLICConfig;
     }
 
-    public void setTensorflowDNNLConfig(TensorflowDNNLConfig tensorflowDNNLConfig) {
-        this.tensorflowDNNLConfig = tensorflowDNNLConfig;
+    public void setTensorflowLICConfig(TensorflowLICConfig tensorflowLICConfig) {
+        this.tensorflowLICConfig = tensorflowLICConfig;
     }
 
-    public TensorflowLConfig getTensorflowLConfig() {
-        return tensorflowLConfig;
+    public TensorflowLIRConfig getTensorflowLIRConfig() {
+        return tensorflowLIRConfig;
     }
 
-    public void setTensorflowLConfig(TensorflowLConfig tensorflowLConfig) {
-        this.tensorflowLConfig = tensorflowLConfig;
+    public void setTensorflowLIRConfig(TensorflowLIRConfig tensorflowLIRConfig) {
+        this.tensorflowLIRConfig = tensorflowLIRConfig;
+    }
+
+    public TensorflowPredictorLSTMConfig getTensorflowPredictorLSTMConfig() {
+        return tensorflowPredictorLSTMConfig;
+    }
+
+    public void setTensorflowPredictorLSTMConfig(TensorflowPredictorLSTMConfig tensorflowPredictorLSTMConfig) {
+        this.tensorflowPredictorLSTMConfig = tensorflowPredictorLSTMConfig;
+    }
+
+    public TensorflowMLPConfig getTensorflowMLPConfig() {
+        return tensorflowMLPConfig;
+    }
+
+    public void setTensorflowMLPConfig(TensorflowMLPConfig tensorflowMLPConfig) {
+        this.tensorflowMLPConfig = tensorflowMLPConfig;
+    }
+
+    public TensorflowCNNConfig getTensorflowCNNConfig() {
+        return tensorflowCNNConfig;
+    }
+
+    public void setTensorflowCNNConfig(TensorflowCNNConfig tensorflowCNNConfig) {
+        this.tensorflowCNNConfig = tensorflowCNNConfig;
+    }
+
+    public TensorflowRNNConfig getTensorflowRNNConfig() {
+        return tensorflowRNNConfig;
+    }
+
+    public void setTensorflowRNNConfig(TensorflowRNNConfig tensorflowRNNConfig) {
+        this.tensorflowRNNConfig = tensorflowRNNConfig;
+    }
+
+    public TensorflowLSTMConfig getTensorflowLSTMConfig() {
+        return tensorflowLSTMConfig;
+    }
+
+    public void setTensorflowLSTMConfig(TensorflowLSTMConfig tensorflowLSTMConfig) {
+        this.tensorflowLSTMConfig = tensorflowLSTMConfig;
+    }
+
+    public TensorflowGRUConfig getTensorflowGRUConfig() {
+        return tensorflowGRUConfig;
+    }
+
+    public void setTensorflowGRUConfig(TensorflowGRUConfig tensorflowGRUConfig) {
+        this.tensorflowGRUConfig = tensorflowGRUConfig;
+    }
+
+    public PytorchMLPConfig getPytorchMLPConfig() {
+        return pytorchMLPConfig;
+    }
+
+    public void setPytorchMLPConfig(PytorchMLPConfig pytorchMLPConfig) {
+        this.pytorchMLPConfig = pytorchMLPConfig;
+    }
+
+    public PytorchCNNConfig getPytorchCNNConfig() {
+        return pytorchCNNConfig;
+    }
+
+    public void setPytorchCNNConfig(PytorchCNNConfig pytorchCNNConfig) {
+        this.pytorchCNNConfig = pytorchCNNConfig;
+    }
+
+    public PytorchRNNConfig getPytorchRNNConfig() {
+        return pytorchRNNConfig;
+    }
+
+    public void setPytorchRNNConfig(PytorchRNNConfig pytorchRNNConfig) {
+        this.pytorchRNNConfig = pytorchRNNConfig;
+    }
+
+    public PytorchLSTMConfig getPytorchLSTMConfig() {
+        return pytorchLSTMConfig;
+    }
+
+    public void setPytorchLSTMConfig(PytorchLSTMConfig pytorchLSTMConfig) {
+        this.pytorchLSTMConfig = pytorchLSTMConfig;
+    }
+
+    public PytorchGRUConfig getPytorchGRUConfig() {
+        return pytorchGRUConfig;
+    }
+
+    public void setPytorchGRUConfig(PytorchGRUConfig pytorchGRUConfig) {
+        this.pytorchGRUConfig = pytorchGRUConfig;
+    }
+
+    public GemSConfig getGemSConfig() {
+        return gemSConfig;
+    }
+
+    public void setGemSConfig(GemSConfig gemSConfig) {
+        this.gemSConfig = gemSConfig;
+    }
+
+    public GemIConfig getGemIConfig() {
+        return gemIConfig;
+    }
+
+    public void setGemIConfig(GemIConfig gemIConfig) {
+        this.gemIConfig = gemIConfig;
+    }
+
+    public GemMMConfig getGemMMConfig() {
+        return gemMMConfig;
+    }
+
+    public void setGemMMConfig(GemMMConfig gemMConfig) {
+        this.gemMMConfig = gemMConfig;
+    }
+
+    public GemEWCConfig getGemEWCConfig() {
+        return gemEWCConfig;
+    }
+
+    public void setGemEWCConfig(GemEWCConfig gemEWCConfig) {
+        this.gemEWCConfig = gemEWCConfig;
+    }
+
+    public GemGEMConfig getGemGEMConfig() {
+        return gemGEMConfig;
+    }
+
+    public void setGemGEMConfig(GemGEMConfig gemGEMConfig) {
+        this.gemGEMConfig = gemGEMConfig;
+    }
+
+    public GemIcarlConfig getGemICarlConfig() {
+        return gemIcarlConfig;
+    }
+
+    public void setGemIcarlConfig(GemIcarlConfig gemIcarlConfig) {
+        this.gemIcarlConfig = gemIcarlConfig;
     }
 
     public LearnTestClassify() {
