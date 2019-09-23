908d4dfcf0530105da2769a0726c7720582a9bf4
Roar Thronæs <roart@nvg.ntnu.no>
2019-09-23 09:56:33 +0200

    Don't need these separate configs, refactor out common Iclij find/improveprofit code (I14).

--- iclij/iclij-core/src/main/java/roart/component/ImproveProfitComponentMLAggregator.java
+++
@@ -1,9 +0,0 @@
-package roart.component; 
-
-import roart.common.util.JsonUtil;
-import roart.component.model.ComponentData;
-import roart.evolution.config.EvolutionConfig;
-
-public class ImproveProfitComponentMLAggregator extends ComponentMLAggregator { 
-    
-}
