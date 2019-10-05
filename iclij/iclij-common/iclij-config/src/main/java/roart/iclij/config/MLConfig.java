b95512545ade73a18f180231b4742f61982c07d0
Roar Thronæs <roart@nvg.ntnu.no>
2019-10-05 12:41:38 +0200

    Remove this, use only enable, for Machine learning persist (I114).

--- iclij/iclij-common/iclij-config/src/main/java/roart/iclij/config/MLConfig.java
+++ iclij/iclij-common/iclij-config/src/main/java/roart/iclij/config/MLConfig.java
@@ -8,9 +8,6 @@ public class MLConfig {
     // enable evolve or use
     private Boolean enable;
 
-    // ml persistence
-    private Boolean persistml;
-    
     public Boolean getLoad() {
         return load;
     }
@@ -25,14 +22,6 @@ public class MLConfig {
 
     public void setEnable(Boolean enable) {
         this.enable = enable;
-    }
-
-    public Boolean getPersistml() {
-        return persistml;
-    }
-
-    public void setPersistml(Boolean persistml) {
-        this.persistml = persistml;
     }
 
     public void merge(MLConfig other) {
