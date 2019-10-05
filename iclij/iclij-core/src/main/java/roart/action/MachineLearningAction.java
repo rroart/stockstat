05db2351507e8da01ef49b0d0529af5370797aca
Roar Thronæs <roart@nvg.ntnu.no>
2019-10-05 12:45:20 +0200

    Machine learning action (I114).

--- iclij/iclij-core/src/main/java/roart/action/MachineLearningAction.java
+++ iclij/iclij-core/src/main/java/roart/action/MachineLearningAction.java
@@ -1,25 +1,111 @@
 package roart.action;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import roart.common.config.ConfigConstants;
+import roart.common.constants.Constants;
+import roart.common.pipeline.PipelineConstants;
 import roart.component.Component;
+import roart.component.ComponentFactory;
+import roart.component.MachineLearningComponentFactory;
 import roart.component.model.ComponentData;
+import roart.component.model.MLIndicatorData;
 import roart.config.Market;
 import roart.constants.IclijConstants;
 import roart.iclij.config.IclijConfig;
+import roart.iclij.config.IclijConfigConstants;
 import roart.iclij.model.IncDecItem;
 import roart.iclij.model.MemoryItem;
+import roart.result.model.ResultMeta;
 import roart.service.model.ProfitData;
 import roart.service.model.ProfitInputData;
+import roart.util.ServiceUtil;
+import roart.util.ServiceUtilConstants;
 
 public class MachineLearningAction extends MarketAction {
 
     private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    protected ProfitInputData filterMemoryListMapsWithConfidence(Market market,
+            Map<Object[], List<MemoryItem>> listMap) {
+        Map<Object[], List<MemoryItem>> badListMap = new HashMap<>();
+        Map<Object[], Double> badConfMap = new HashMap<>();
+        for(Object[] keys : listMap.keySet()) {
+            List<MemoryItem> memoryList = listMap.get(keys);
+            List<Double> confidences = memoryList.stream().map(MemoryItem::getConfidence).collect(Collectors.toList());
+            confidences = confidences.stream().filter(m -> m != null && !m.isNaN()).collect(Collectors.toList());
+            Optional<Double> minOpt = confidences.parallelStream().reduce(Double::min);
+            Double min = 0.0;
+            if (minOpt.isPresent()) {
+                min = minOpt.get();
+            }
+            badListMap.put(keys, listMap.get(keys));
+            badConfMap.put(keys, min);
+        }
+        ProfitInputData input = new ProfitInputData();
+        input.setConfMap(badConfMap);
+        input.setListMap(badListMap);
+        return input;
+    }
+
+    @Override
+    protected void handleComponent(MarketAction action, Market market, ProfitData profitdata, ComponentData param,
+            Map<String, List<Integer>> listComponent, Map<String, Component> componentMap,
+            Map<String, ComponentData> dataMap, Boolean buy, String subcomponent, WebData myData, IclijConfig config) {
+        for (Entry<String, List<Integer>> entry : listComponent.entrySet()) {
+            List<Integer> positions = entry.getValue();
+            String componentName = entry.getKey();
+            Component component = componentMap.get(componentName);
+            if (component == null) {
+                continue;
+            }
+            //if (dataMap.containsKey(componentName)) {
+            //    param = dataMap.get(componentName);
+            //}
+            //component.enableDisable(param, positions, param.getConfigValueMap());
+
+            // 0 ok?
+            param.getConfigValueMap().put(ConfigConstants.MISCMYTABLEDAYS, 0);
+            param.getConfigValueMap().put(ConfigConstants.MISCMYDAYS, 0);
+            param.getConfigValueMap().put(IclijConfigConstants.FINDPROFITMLDYNAMIC, Boolean.FALSE);
+            Map<String, Object> aMap = new HashMap<>();
+            aMap.put(ConfigConstants.MACHINELEARNINGMLDYNAMIC, false);
+            aMap.put(ConfigConstants.MACHINELEARNINGMLCLASSIFY, false);
+            aMap.put(ConfigConstants.MACHINELEARNINGMLLEARN, true);
+            aMap.put(ConfigConstants.MISCMYTABLEDAYS, 0);
+            aMap.put(ConfigConstants.MISCMYDAYS, 0);
+            
+            boolean evolve = false; // param.getInput().getConfig().wantEvolveML();
+            //component.set(market, param, profitdata, positions, evolve);
+            ComponentData componentData = component.handle(action, market, param, profitdata, positions, evolve, aMap, subcomponent);
+        }
+    }
+
+    @Override
+    protected List<IncDecItem> getIncDecItems() {
+        return null;
+    }
+
+    @Override
+    protected List getAnArray() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected Boolean getBool() {
+        return true;
+    }
 
     @Override
     public String getName() {
@@ -27,78 +113,44 @@ public class MachineLearningAction extends MarketAction {
     }
 
     @Override
-    protected ProfitInputData filterMemoryListMapsWithConfidence(Market market,
-            Map<Object[], List<MemoryItem>> listMap) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    protected void handleComponent(Market market, ProfitData profitdata, ComponentData param,
-            Map<String, List<Integer>> listComponent, Map<String, Component> componentMap,
-            Map<String, ComponentData> dataMap, Boolean buy, String subcomponent, WebData myData) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    protected List<IncDecItem> getIncDecItems() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    protected List getAnArray() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    protected Boolean getBool() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
     protected List<String> getProfitComponents(IclijConfig config, String marketName) {
-        // TODO Auto-generated method stub
-        return null;
+        return ServiceUtil.getMachineLearningComponents(config, marketName);
     }
 
     @Override
-    protected Short getTime(Market market) {
-        // TODO Auto-generated method stub
-        return null;
+    public Short getTime(Market market) {
+        return market.getConfig().getPersisttime();
     }
 
     @Override
-    protected Boolean[] getBooleans() {
-        // TODO Auto-generated method stub
-        return null;
+    public Boolean[] getBooleans() {
+        return new Boolean[] { null };
     }
 
     @Override
     protected boolean getEvolve(Component component, ComponentData param) {
-        // TODO Auto-generated method stub
-        return false;
+        return component.wantEvolve(param.getInput().getConfig());
     }
 
     @Override
     protected List<MemoryItem> getMemItems(MarketComponentTime marketTime, WebData myData, ComponentData param,
             IclijConfig config, Boolean evolve, Map<String, ComponentData> dataMap) {
-        // TODO Auto-generated method stub
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
     protected LocalDate getPrevDate(ComponentData param, Market market) {
-        // TODO Auto-generated method stub
-        return null;
+        return param.getInput().getEnddate();
     }
 
     @Override
     protected void setValMap(ComponentData param) {
-        // TODO Auto-generated method stub
-        
+        param.getAndSetCategoryValueMap();
     }
+    
+    @Override
+    public ComponentFactory getComponentFactory() {
+        return new MachineLearningComponentFactory();
+    }
+
 }
