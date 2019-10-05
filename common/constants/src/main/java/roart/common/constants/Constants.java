74227610ebc282fcb0aec854d796158c4abacdfe
Roar Thronæs <roart@nvg.ntnu.no>
2019-10-05 13:21:32 +0200

    Machine learning persist (I114).

--- common/constants/src/main/java/roart/common/constants/Constants.java
+++ common/constants/src/main/java/roart/common/constants/Constants.java
@@ -5,52 +5,62 @@ public class Constants {
     public static final int PERIODS = 9;
     public static final int ALLPERIODS = 11;
 
-public static final String EXCEPTION = "Exception";
-public static final String ERROR = "Error";
+    public static final String EXCEPTION = "Exception";
+    public static final String ERROR = "Error";
+    public static final String NOTFOUND = "Not found {}";
 
-public static final String USER = "user";
-public static final String NONE = "none";
-public static final String ADMIN = "admin";
-public static final String ADDR = "addr";
+    public static final String USER = "user";
+    public static final String NONE = "none";
+    public static final String ADMIN = "admin";
+    public static final String ADDR = "addr";
 
-public static final String AETHER = "aether";
-public static final String LOCK = "lock";
-public static final String NODES = "nodes";
-public static final String REFRESH = "refresh";
-public static final String QUEUE = "queue";
-public static final String DB = "db";
+    public static final String AETHER = "aether";
+    public static final String LOCK = "lock";
+    public static final String NODES = "nodes";
+    public static final String REFRESH = "refresh";
+    public static final String QUEUE = "queue";
+    public static final String DB = "db";
 
-public static final String ZKDELIMITER = "-";
+    public static final String ZKDELIMITER = "-";
 
-public static final String FILESCONTENTTYPE = "Files-Content-Type";
+    public static final String FILESCONTENTTYPE = "Files-Content-Type";
 
-public static final String INDEXCOUNT = "indexcount";
+    public static final String INDEXCOUNT = "indexcount";
 
-public static final String MYDATEFORMAT = "yyyy.MM.dd";
+    public static final String MYDATEFORMAT = "yyyy.MM.dd";
 
-public static final String IMG = "Img";
+    public static final String IMG = "Img";
 
-public static final int PRICECOLUMN = -1;
-public static final int INDEXVALUECOLUMN = -2;
+    public static final int PRICECOLUMN = -1;
+    public static final int INDEXVALUECOLUMN = -2;
 
-public static final int FULLSIZE = 100;
-public static final int INDICATORSIZE = 50;
+    public static final int FULLSIZE = 100;
+    public static final int INDICATORSIZE = 50;
 
-public static final String PRICE = "Price";
-public static final String INDEX = "Index";
+    public static final String PRICE = "Price";
+    public static final String INDEX = "Index";
 
-public static final String DELTA = "Δ";
+    public static final String DELTA = "Δ";
 
-public static final String WEBBR = "<br/>";
+    public static final String WEBBR = "<br/>";
 
-public static final String TP = "TP";
-public static final String TN = "TN";
-public static final String FP = "FP";
-public static final String FN = "FN";
+    public static final String TP = "TP";
+    public static final String TN = "TN";
+    public static final String FP = "FP";
+    public static final String FN = "FN";
 
-public static final String INC = "Inc";
-public static final String DEC = "Dec";
-public static final int EVENT = 0;
-public static final int MLTIMES = 1;
+    public static final String INC = "Inc";
+    public static final String DEC = "Dec";
+    public static final int EVENT = 0;
+    public static final int MLTIMES = 1;
 
+    public static final String MACD = "macd";
+    public static final String HIST = "hist";
+    public static final String SIGNAL = "signal";
+    public static final String RSI = "rsi";
+    public static final String ATR = "atr";
+    public static final String CCI = "cci";
+    public static final String STOCH = "stoch";
+    public static final String STOCHRSI = "stochrsi";
+    public static final String MOVE = "move";
 }
