ec63a477f4723dd4f395795c7f705918e6e0f5aa
Roar Thronæs <roart@nvg.ntnu.no>
2019-10-09 14:05:28 +0200

    Typo fix, pytorch (I99).

--- main/ml/pytorch/src/main/java/roart/ml/pytorch/MLClassifyPytorchCNNModel.java
+++ main/ml/pytorch/src/main/java/roart/ml/pytorch/MLClassifyPytorchCNNModel.java
@@ -25,7 +25,7 @@ public class MLClassifyPytorchCNNModel  extends MLClassifyPytorchModel {
     
     @Override
     public String getKey() {
-        return ConfigConstants.MACHINELEARNINGTENSORFLOWCNNCONFIG;
+        return ConfigConstants.MACHINELEARNINGPYTORCHCNNCONFIG;
     }
 
     @Override
