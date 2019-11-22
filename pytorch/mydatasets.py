11e7d93e0c287a0a25ec2672cb316de3804b561c
Roar Thronæs <roart@nvg.ntnu.no>
2019-11-22 11:07:36 +0100

    Add new datasets (I142).

--- pytorch/mydatasets.py
+++ pytorch/mydatasets.py
@@ -14,17 +14,21 @@ def getdataset(myobj, config, classifier):
     if myobj.dataset == 'cifar10':
         return getcifar10(config)
     if myobj.dataset == 'dailymintemperatures':
-        return getdailymintemperatures(myobj, config, classifier)
-
+        return getdailymintemperatures(myobj, config)
+    if myobj.dataset == 'nasdaq':
+        return getnasdaq(myobj, config)
+    if myobj.dataset == 'number':
+        return getnumber(myobj, config)
+    
 def getmnist(config):
     dl = DataLoader(torchvision.datasets.MNIST('/tmp/datasets/mnist', train=True, download=True))
 
     tensor = dl.dataset.data
     tensor = tensor.to(dtype=torch.float32)
-    print("tt", tensor.shape)
+    #print("tt", tensor.shape)
     tr = tensor
     #.reshape(tensor.size(0), -1) 
-    print("tt", tr.shape)
+    #print("tt", tr.shape)
     tr = tr/128
     targets = dl.dataset.targets
     targets = targets.to(dtype=torch.float)
@@ -47,7 +51,7 @@ def getmnist(config):
     #loaders={}
     #loaders['train'] = train_dl
     #loaders['valid'] = valid_dl
-    print("X", x_train.shape)
+    #print("X", x_train.shape)
     mydim = (x_train.shape[1], x_train.shape[2])
     if not config.name == "cnn" and not config.name == "cnn2":
       if config.name == "rnn" or config.name == "lstm" or config.name == "gru":
@@ -58,7 +62,7 @@ def getmnist(config):
         #x_train = x_train.view(-1, 28, 28)
         #x_valid = x_valid.view(-1, 28, 28)
         mydim = 28
-        print("here")
+        #print("here")
       else:
         x_train = x_train.reshape(x_train.size(0), -1)
         x_valid = x_valid.reshape(x_valid.size(0), -1)
@@ -67,28 +71,29 @@ def getmnist(config):
       #mydim = 28
       mydim = mydim
       if config.name == "cnn2":
-          print("here21")
+          #print("here21")
           mydim = (28, 28, 1)
           #x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
           #x_valid = x_valid.reshape(x_valid.shape[0], 28, 28, 1)
           x_train = x_train.reshape(x_train.shape[0], 1, 28, 28)
           x_valid = x_valid.reshape(x_valid.shape[0], 1, 28, 28)
           mydim = (1, 28, 28)
-    print(type(x_train), x_train.shape)
-    print(type(y_train), y_train.shape)
-    print("mydim", mydim)
+    #print(type(x_train), x_train.shape)
+    #print(type(y_train), y_train.shape)
+    #print("mydim", mydim)
     return x_train, y_train, x_valid, y_valid, mydim, 10, True
 
 def getcifar10(config):
     dl = DataLoader(torchvision.datasets.CIFAR10('/tmp/datasets/cifar10', train=True, download=True))
 
     tensor = dl.dataset.data
+    #print(tensor.shape)
     tensor = torch.FloatTensor(tensor)
     tensor = tensor.to(dtype=torch.float32)
-    print("tt", tensor.shape)
+    #print("tt", tensor.shape)
     tr = tensor
     #.reshape(tensor.size(0), -1) 
-    print("tt", tr.shape)
+    #print("tt", tr.shape)
     tr = tr/128
     targets = dl.dataset.targets
     targets = torch.FloatTensor(targets)
@@ -112,7 +117,7 @@ def getcifar10(config):
     #loaders={}
     #loaders['train'] = train_dl
     #loaders['valid'] = valid_dl
-    print("X", x_train.shape)
+    #print("X", x_train.shape)
     mydim = (x_train.shape[1], x_train.shape[2])
     if not config.name == "cnn" and not config.name == "cnn2":
       if config.name == "rnn" or config.name == "lstm" or config.name == "gru":
@@ -123,9 +128,9 @@ def getcifar10(config):
         #x_train = x_train.view(-1, 32, 32)
         #x_valid = x_valid.view(-1, 32, 32)
         mydim = 32
-        print("here")
+        #print("here")
       else:
-        print(x_train.shape)
+        #print(x_train.shape)
         x_train = x_train.reshape(x_train.size(0), 3072)
         x_valid = x_valid.reshape(x_valid.size(0), 3072)
         mydim = 3072
@@ -133,31 +138,67 @@ def getcifar10(config):
       #mydim = 32
       mydim = mydim
       if config.name == "cnn2":
-          print("here21")
+          #print("here21")
           mydim = (32, 32, 1)
           #x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
           #x_valid = x_valid.reshape(x_valid.shape[0], 28, 28, 1)
           x_train = x_train.reshape(x_train.shape[0], 3, 32, 32)
           x_valid = x_valid.reshape(x_valid.shape[0], 3, 32, 32)
           mydim = (3, 32, 32)
-    print(type(x_train), x_train.shape)
-    print(type(y_train), y_train.shape)
-    print(type(x_valid), x_valid.shape)
-    print("mydim", mydim)
+    #print(type(x_train), x_train.shape)
+    #print(type(y_train), y_train.shape)
+    #print(type(x_valid), x_valid.shape)
+    #print("mydim", mydim)
     return x_train, y_train, x_valid, y_valid, mydim, 10, True
 
-def getdailymintemperatures(myobj, config, classifier):
+def getdailymintemperatures(myobj, config):
     url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv'
     torchvision.datasets.utils.download_url(url, "/tmp/")
     file_path = "/tmp/daily-min-temperatures.csv"
     data = np.genfromtxt(file_path, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'temp'), 'formats': (np.str, np.float)})
-    print(type(data), data.shape, data)
+    #print(type(data), data.shape, data)
     data = data['temp']
     #data = data[1:20]
-    data = [[data]]
-    print(type(data), len(data), data)
-    (inputs, labels) = classifier.getSlide(data, None, myobj, config)
-    #print("iiiiiiiiii")
-    #print(inputs)
-    #print(labels)
-    return inputs, labels, inputs, labels, myobj.size, myobj.classes, False
+    data = [ data ]
+    #print(type(data), len(data), data)
+
+    return data, None, data, None, myobj.size, myobj.classes, False
+
+def getnasdaq(myobj, config):
+    url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=NASDAQCOM&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05'
+    torchvision.datasets.utils.download_url(url, "/tmp/")
+    file_path = "/tmp/fredgraph.csv?mode=fred&id=NASDAQCOM&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05"
+    data = np.genfromtxt(file_path, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'nasdaqcom'), 'formats': (np.str, np.float)})
+    #print(type(data), data.shape, data)
+    data = data['nasdaqcom']
+    data = data[np.logical_not(np.isnan(data))]
+    #data = data[1:20]
+
+    url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=DJIA&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05'
+    torchvision.datasets.utils.download_url(url, "/tmp/")
+    file_path2 = "/tmp/fredgraph.csv?mode=fred&id=DJIA&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05"
+    data2 = np.genfromtxt(file_path2, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'djia'), 'formats': (np.str, np.float)})
+    #print(type(data2), data2.shape, data2)
+    data2 = data2['djia']
+    data2 = data2[np.logical_not(np.isnan(data2))]
+    #data = data[1:20]
+
+    #print(type(data), len(data), data)
+    #print(type(data2), len(data2), data2)
+
+    data3 = 100 * data / data[0]
+    data4 = 100 * data2 / data2[0]
+    #print(data3[0:10])
+    
+    data = [ data ]
+    data2 = [ data2 ]
+
+    return data, None, data, None, myobj.size, myobj.classes, False
+
+def getnumber(myobj, config):
+    data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 ]
+    data1 = [ 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140 ]
+
+    data = [ data, data1 ]
+
+    return data, None, data, None, myobj.size, myobj.classes, False
--- tensorflow/mydatasets.py
+++ tensorflow/mydatasets.py
@@ -3,18 +3,22 @@ import numpy as np
 #import pandas as pd 
 from keras import backend as K
 
-def getdataset(myobj, config):
+def getdataset(myobj, config, classifier):
     if myobj.dataset == 'mnist':
         return getmnist(config)
     if myobj.dataset == 'cifar10':
         return getcifar10(config)
     if myobj.dataset == 'dailymintemperatures':
         return getdailymintemperatures(myobj)
+    if myobj.dataset == 'nasdaq':
+        return getnasdaq(myobj, config)
+    if myobj.dataset == 'number':
+        return getnumber(myobj, config)
 
 def getmnist(config):
     #load mnist data
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data("/tmp/datasets/mnist.npz")
-    print(tf.keras.backend.image_data_format())
+    #print(tf.keras.backend.image_data_format())
     def create_mnist_dataset(data, labels, batch_size):
         def gen():
             for image, label in zip(data, labels):
@@ -24,20 +28,20 @@ def getmnist(config):
     #train and validation dataset with different batch size
     train_dataset = create_mnist_dataset(x_train, y_train, 10)
     valid_dataset = create_mnist_dataset(x_test, y_test, 20)
-    print(type(x_train))
-    print(type(y_train))
-    print(type(train_dataset))
-    print(x_train.shape)
-    print(x_train.shape, x_train.shape[2])
-    print(type(y_train.shape))
+    #print(type(x_train))
+    #print(type(y_train))
+    #print(type(train_dataset))
+    #print(x_train.shape)
+    #print(x_train.shape, x_train.shape[2])
+    #print(type(y_train.shape))
     mydim = (x_train.shape[1], x_train.shape[2])
     if not config.name == "cnn" and not config.name == "cnn2":
         if config.name == "rnn" or config.name == "lstm" or config.name == "gru":
-            print("here")
+            #print("here")
             if config.name == "rnnnot":
-                print(x_train.shape)
+                #print(x_train.shape)
                 x_train = np.transpose(x_train, [1, 0, 2])
-                print(x_train.shape)
+                #print(x_train.shape)
                 x_test = np.transpose(x_test, [1, 0, 2])
             #x_train = x_train.reshape(1, x_train.shape[0], x_train.shape[1])
             #x_test = x_test.reshape(1, x_test.shape[0], x_test.shape[1])
@@ -51,10 +55,11 @@ def getmnist(config):
         if config.name == "cnn2":
             mydim = (28, 28, 1)
             x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-            x_test = np.float16(x_test.reshape(x_test.shape[0], 28, 28, 1))
-    print(x_train.shape)
-    print(y_train.shape)
-    return x_train, y_train, x_test, y_test, mydim, 10
+            x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    x_test = np.float16(x_test)        
+    #print(x_train.shape)
+    #print(y_train.shape)
+    return x_train, y_train, x_test, y_test, mydim, 10, True
 
 def getcifar10(config):
     #load mnist data
@@ -69,20 +74,20 @@ def getcifar10(config):
     #train and validation dataset with different batch size
     train_dataset = create_cifar10_dataset(x_train, y_train, 10)
     valid_dataset = create_cifar10_dataset(x_test, y_test, 20)
-    print(type(x_train))
-    print(type(y_train))
-    print(type(train_dataset))
-    print(x_train.shape)
-    print(x_train.shape, x_train.shape[2])
-    print(type(y_train.shape))
+    #print(type(x_train))
+    #print(type(y_train))
+    #print(type(train_dataset))
+    #print(x_train.shape)
+    #print(x_train.shape, x_train.shape[2])
+    #print(type(y_train.shape))
     mydim = (x_train.shape[1], x_train.shape[2])
     if not config.name == "cnn" and not config.name == "cnn2":
         if config.name == "rnn" or config.name == "lstm" or config.name == "gru":
-            print("here")
+            #print("here")
             if config.name == "rnnnot":
-                print(x_train.shape)
+                #print(x_train.shape)
                 x_train = np.transpose(x_train, [1, 0, 2])
-                print(x_train.shape)
+                #print(x_train.shape)
                 x_test = np.transpose(x_test, [1, 0, 2])
             #x_train = x_train.reshape(1, x_train.shape[0], x_train.shape[1])
             #x_test = x_test.reshape(1, x_test.shape[0], x_test.shape[1])
@@ -94,22 +99,63 @@ def getcifar10(config):
         y_test = np.int32(y_test)
     else:
         if config.name == "cnn2":
-            print("here21")
-            print(x_train.shape)
+            #print("here21")
+            #print(x_train.shape)
             mydim = (32, 32, 3)
             x_train = x_train.reshape(x_train.shape[0], 32, 32, 3)
             x_test = np.float16(x_test.reshape(x_test.shape[0], 32, 32, 3))
             #x_train = x_train.reshape(x_train.shape[0], 1, 32, 32)
             #x_test = x_test.reshape(x_test.shape[0], 1, 32, 32)
             #mydim = (1, 32, 32)
-    print(x_train.shape)
-    print(y_train.shape)
-    return x_train, y_train, x_test, y_test, mydim, 10
+    #print(x_train.shape)
+    #print(y_train.shape)
+    return x_train, y_train, x_test, y_test, mydim, 10, True
 
 def getdailymintemperatures(myobj):
     url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/daily-min-temperatures.csv'
     file_path = tf.keras.utils.get_file("/tmp/daily-min-temperatures.csv", url)
     data = np.genfromtxt(file_path, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'temp'), 'formats': (np.str, np.float)})
     data = data['temp']
-    return data, None, None, None, myobj.size, myobj.classes
+    data = [ data ]
+    #print(type(data), len(data), data)
+    return data, None, data, None, myobj.size, myobj.classes, False
+#    return data, None, None, None, myobj.size, myobj.classes
     
+def getnasdaq(myobj, config):
+    url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=NASDAQCOM&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05'
+    file_path = tf.keras.utils.get_file("/tmp/", url)
+    file_path = "/tmp/fredgraph.csv?mode=fred&id=NASDAQCOM&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05"
+    data = np.genfromtxt(file_path, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'nasdaqcom'), 'formats': (np.str, np.float)})
+    #print(type(data), data.shape, data)
+    data = data['nasdaqcom']
+    data = data[np.logical_not(np.isnan(data))]
+    #data = data[1:20]
+
+    url = 'https://fred.stlouisfed.org/graph/fredgraph.csv?mode=fred&id=DJIA&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05'
+    file_path2 = tf.keras.utils.get_file("/tmp/", url)
+    file_path2 = "/tmp/fredgraph.csv?mode=fred&id=DJIA&cosd=2014-11-15&coed=2019-11-15&vintage_date=2019-11-17&revision_date=2019-11-17&nd=1971-02-05"
+    data2 = np.genfromtxt(file_path2, delimiter = ',', skip_header = 1, dtype = {'names': ('date', 'djia'), 'formats': (np.str, np.float)})
+    #print(type(data2), data2.shape, data2)
+    data2 = data2['djia']
+    data2 = data2[np.logical_not(np.isnan(data2))]
+    #data = data[1:20]
+
+    #print(type(data), len(data), data)
+    #print(type(data2), len(data2), data2)
+
+    data3 = 100 * data / data[0]
+    data4 = 100 * data2 / data2[0]
+    #print(data3[0:10])
+    
+    data = [ data ]
+    data2 = [ data2 ]
+
+    return data, None, data, None, myobj.size, myobj.classes, False
+
+def getnumber(myobj, config):
+    data = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 ]
+    data1 = [ 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140 ]
+
+    data = [ data, data1 ]
+
+    return data, None, data, None, myobj.size, myobj.classes, False
