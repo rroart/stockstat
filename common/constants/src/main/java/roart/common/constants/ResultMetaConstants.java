e374f00fa8fbd15ffb4d468ccd78cedefdb6c9b1
Roar Thronæs <roart@nvg.ntnu.no>
2019-12-30 09:50:24 +0100

    ResultMeta use change, with numbered array (I154).

---
+++ common/constants/src/main/java/roart/common/constants/ResultMetaConstants.java
@@ -0,0 +1,31 @@
+package roart.common.constants;
+
+public class ResultMetaConstants {
+    
+    public static final int MLNAME = 0;
+    
+    public static final int MODELNAME = 1;
+    
+    public static final int RETURNSIZE = 2;
+    
+    public static final int SUBTYPE = 3;
+    
+    public static final int SUBSUBTYPE = 4;
+
+    public static final int LEARNMAP = 5;
+    
+    public static final int TESTACCURACY = 6;
+    
+    public static final int CLASSIFYMAP = 7;
+    
+    public static final int COUNTMAP = 8;
+    
+    public static final int OFFSETMAP = 9;
+    
+    public static final int LOSS = 10;
+    
+    public static final int THRESHOLD = 11;
+    
+    public static final int SIZE = 12;
+    
+}
