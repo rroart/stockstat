8e70e408c7704a2bec03032fa103a100c8541f8c
Roar Thronæs <roart@nvg.ntnu.no>
2020-01-17 14:46:44 +0100

    Marketfilter action (I158).

---
+++ iclij/iclij-core/src/main/java/roart/component/ImproveFilterComponentMLATR.java
@@ -0,0 +1,26 @@
+package roart.component;
+
+import roart.common.util.JsonUtil;
+import roart.component.model.ComponentData;
+import roart.evolution.config.EvolutionConfig;
+import roart.iclij.config.IclijConfigConstants;
+import roart.iclij.config.IclijConfig;
+
+public class ImproveFilterComponentMLATR extends ComponentMLATR {
+    @Override
+    public EvolutionConfig getLocalEvolutionConfig(ComponentData componentdata) {
+        String localEvolve = componentdata.getInput().getConfig().getImproveFilterMLATREvolutionConfig();
+        return JsonUtil.convert(localEvolve, EvolutionConfig.class);
+    }
+
+    @Override
+    public String getLocalMLConfig(ComponentData componentdata) {
+        return componentdata.getInput().getConfig().getImproveFilterMLATRMLConfig();
+    }
+
+    @Override
+    public int getPriority(IclijConfig srv) {
+        return getPriority(srv, IclijConfigConstants.IMPROVEFILTERMLATR);
+    }
+
+}
---
+++ iclij/iclij-core/src/main/java/roart/component/ImproveFilterComponentRecommender.java
@@ -0,0 +1,3 @@
+package roart.component;
+public class ImproveFilterComponentRecommender extends ComponentRecommender {
+}
