e3b13a172e707ff801ee331e9f77b6d16c2e7347
Roar Thronæs <roart@nvg.ntnu.no>
2020-04-10 08:48:50 +0200

    Undo.

--- common/model/src/main/java/module-info.java
+++ common/model/src/main/java/module-info.java
@@ -7,7 +7,6 @@
  */
 module common.model {
     exports roart.common.model;
-    exports roart.common.model.ml;
     exports roart.common.pipeline.model;
     exports roart.result.model;
     requires common.constants;
