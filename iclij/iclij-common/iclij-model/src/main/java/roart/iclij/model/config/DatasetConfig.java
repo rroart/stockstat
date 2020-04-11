a7132147255dbf56b336141cc7bf775160640d48
Roar Thronæs <roart@nvg.ntnu.no>
2020-04-11 09:05:44 +0200

    Split out pure iclij web from iclij-core (I180).

---
+++ iclij/iclij-common/iclij-model/src/main/java/roart/iclij/model/config/DatasetConfig.java
@@ -0,0 +1,16 @@
+package roart.iclij.model.config;
+
+import roart.iclij.config.IclijConfig;
+
+public class DatasetConfig extends DatasetActionComponentConfig {
+    @Override
+    public String getLocalEvolutionConfig(IclijConfig config) {
+        return null;
+    }
+
+    @Override
+    public String getLocalMLConfig(IclijConfig config) {
+        return null;
+    }
+
+}
--- iclij/iclij-common/iclij-util/src/main/java/roart/iclij/factory/actioncomponentconfig/DatasetActionComponentConfigFactory.java
+++ iclij/iclij-common/iclij-util/src/main/java/roart/iclij/factory/actioncomponentconfig/DatasetActionComponentConfigFactory.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import roart.iclij.model.config.ActionComponentConfig;
+import roart.iclij.model.config.DatasetConfig;
 
 public class DatasetActionComponentConfigFactory extends ActionComponentConfigFactory{
     @Override
     public ActionComponentConfig factory(String component) {
         switch (component) {
         default:
-            return null;
+            return new DatasetConfig();
         }
     }
 
     @Override
     public List<ActionComponentConfig> getAllComponents() {
         List<ActionComponentConfig> list = new ArrayList<>();
+        list.add(new DatasetConfig());
         return list;
     }
 
