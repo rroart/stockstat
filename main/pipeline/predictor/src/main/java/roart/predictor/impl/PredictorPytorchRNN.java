69f64045578e4a119e853859c396f8deff4d01b5
Roar Thronæs <roart@nvg.ntnu.no>
2020-08-07 11:29:15 +0200

    Put the early data pipeline in a class, plus refactor and cleanup (I210).

--- main/pipeline/predictor/src/main/java/roart/predictor/impl/PredictorPytorchRNN.java
+++ main/pipeline/predictor/src/main/java/roart/predictor/impl/PredictorPytorchRNN.java
@@ -7,13 +7,12 @@ import roart.common.config.MyMyConfig;
 import roart.common.ml.NeuralNetCommand;
 import roart.common.pipeline.PipelineConstants;
 import roart.model.data.MarketData;
-import roart.model.data.PeriodData;
 import roart.pipeline.Pipeline;
 
 public class PredictorPytorchRNN extends PytorchPredictor {
 
-    public PredictorPytorchRNN(MyMyConfig conf, String string, Map<String, MarketData> marketdatamap, Map<String, PeriodData> periodDataMap, String title, int category, AbstractCategory[] categories, Pipeline[] datareaders, NeuralNetCommand neuralnetcommand) throws Exception {
-        super(conf, string, category, neuralnetcommand, marketdatamap, periodDataMap, categories, datareaders);
+    public PredictorPytorchRNN(MyMyConfig conf, String string, Map<String, MarketData> marketdatamap, String title, int category, AbstractCategory[] categories, Pipeline[] datareaders, NeuralNetCommand neuralnetcommand) throws Exception {
+        super(conf, string, category, neuralnetcommand, marketdatamap, categories, datareaders);
     }
 
     @Override
--- main/pipeline/predictor/src/main/java/roart/predictor/impl/TensorflowPredictor.java
+++ main/pipeline/predictor/src/main/java/roart/predictor/impl/TensorflowPredictor.java
@@ -7,13 +7,12 @@ import roart.common.config.MLConstants;
 import roart.common.config.MyMyConfig;
 import roart.common.ml.NeuralNetCommand;
 import roart.model.data.MarketData;
-import roart.model.data.PeriodData;
 import roart.pipeline.Pipeline;
 
 public abstract class TensorflowPredictor extends Predictor {
 
-    public TensorflowPredictor(MyMyConfig conf, String string, int category, NeuralNetCommand neuralnetcommand, Map<String, MarketData> marketdatamap, Map<String, PeriodData> periodDataMap, AbstractCategory[] categories, Pipeline[] datareaders) {
-        super(conf, string, category, neuralnetcommand, marketdatamap, periodDataMap, categories, datareaders);
+    public TensorflowPredictor(MyMyConfig conf, String string, int category, NeuralNetCommand neuralnetcommand, Map<String, MarketData> marketdatamap, AbstractCategory[] categories, Pipeline[] datareaders) {
+        super(conf, string, category, neuralnetcommand, marketdatamap, categories, datareaders);
     }
     
     @Override
