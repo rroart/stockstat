72688bc907aaad74ddff00f5a0c57403b6f87bfa
Roar Thronæs <roart@nvg.ntnu.no>
2020-09-29 11:42:11 +0200

    Calculate expression, for gold/copper ratio etc (I196).

---
+++ common/config/src/main/java/roart/common/config/MarketStock.java
@@ -0,0 +1,9 @@
+package roart.common.config;
+
+public class MarketStock {
+    private String market;
+    
+    private String id;
+    
+    private String category;
+}
---
+++ common/config/src/main/java/roart/common/config/MarketStockExpression.java
@@ -0,0 +1,13 @@
+package roart.common.config;
+
+import java.util.List;
+
+public class MarketStockExpression {
+
+    // Inner list:
+    // ( market, id, (eventual) field )
+    private List<MarketStock> items;
+       
+    private String expression;
+   
+}
