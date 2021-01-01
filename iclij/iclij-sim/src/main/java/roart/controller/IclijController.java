3868b67d8f504dda2b9f6bdd4ada24eac1ee3888
Roar Thronæs <roart@nvg.ntnu.no>
2021-01-01 22:44:04 +0100

    Need to process evolution results, for simulate invest action (I220).

---
+++ iclij/iclij-sim/src/main/java/roart/controller/IclijController.java
@@ -0,0 +1,64 @@
+package roart.controller;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import roart.common.cache.MyCache;
+import roart.executor.MyExecutors;
+import roart.iclij.config.IclijConfig;
+import roart.iclij.config.IclijXMLConfig;
+import roart.iclij.service.ControlService;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class IclijController implements CommandLineRunner {
+
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+    
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(IclijController.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws InterruptedException, JsonParseException, JsonMappingException, IOException {	    
+	    System.out.println("Using profile " + activeProfile);
+	    try {
+	        MyExecutors.initThreads("dev".equals(activeProfile));
+            MyExecutors.init(new double[] { IclijXMLConfig.getConfigInstance().mpServerCpu() } );
+            new ServiceControllerOther().start();
+            IclijConfig instance = IclijXMLConfig.getConfigInstance();
+            MyCache.setCache(instance.wantCache());
+            MyCache.setCacheTTL(instance.getCacheTTL());
+	    } catch (Exception e) {
+	        e.printStackTrace();
+
+	    }
+	}
+
+	@Bean(name = "OBJECT_MAPPER_BEAN")
+	public ObjectMapper jsonObjectMapper() {
+	    return Jackson2ObjectMapperBuilder.json()
+	            .serializationInclusion(JsonInclude.Include.NON_NULL)
+	            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+	            .modules(new JavaTimeModule())
+	            .build();
+	}
+
+}
---
+++ iclij/iclij-sim/src/main/java/roart/controller/ServiceController.java
@@ -0,0 +1,54 @@
+package roart.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import roart.common.constants.Constants;
+import roart.common.constants.EurekaConstants;
+import roart.iclij.config.IclijConfig;
+import roart.iclij.config.IclijConfigConstants;
+import roart.iclij.config.IclijXMLConfig;
+import roart.iclij.config.SimulateInvestConfig;
+import roart.iclij.model.component.ComponentInput;
+import roart.iclij.service.ControlService;
+import roart.iclij.service.IclijServiceParam;
+import roart.iclij.service.IclijServiceResult;
+
+@CrossOrigin
+@RestController
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceController {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    private ControlService instance;
+
+    private ControlService getInstance() {
+        if (instance == null) {
+            instance = new ControlService();
+        }
+        return instance;
+    }
+
+    @GetMapping(path = "/")
+    public ResponseEntity healthCheck() throws Exception {
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
