8c7f289e19efef32203f4d67939c8056fe9c8329
Roar Thronæs <roart@nvg.ntnu.no>
2021-01-19 22:58:30 +0100

    Processing other evolution results (I240).

--- config/ievolve/localdev.sh
+++ config/ievolve/localdev.sh
@@ -1,6 +1,6 @@
 export IEVOLVETMPL=/home/roart/src/stockstatspark/config/ievolve/ievolve.xml.tmpl
 export CACHE=true
 export CACHETTL=600
-export MYSERVICESIEVOLVE="{ \"filterevolve\" : \"\" }"
+export MYSERVICESIEVOLVE="{ \"evolvefilterevolve\" : \"\" }"
 #export SERVICES="{ \"filterinvest\" : \"spring\" }"
 #export COMMUNICATIONS="{ \"spring\" : \"localhost\" }"
