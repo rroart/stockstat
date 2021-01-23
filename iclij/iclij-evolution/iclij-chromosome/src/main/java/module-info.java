61f915df7c5e8eb4c7abd35e47955bac4b80b0e1
Roar Thronæs <roart@nvg.ntnu.no>
2021-01-23 21:58:14 +0100

    Move to subproject, Jackson annotations and remove classes, evolution refactor (I111).

---
+++ iclij/iclij-evolution/iclij-chromosome/src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+/**
+ * @author roart
+ *
+ */
+module iclij.evolutionchromosome {
+    exports roart.iclij.evolution.marketfilter.chromosome.impl;
+    exports roart.iclij.evolution.chromosome.impl;
+    exports roart.iclij.evolution.chromosome.winner;
+    requires iclij.common.config;
+    requires iclij.common.componentdata;
+    requires iclij.evolutionmarketfilter;
+    requires iclij.evolutioniclijconfigmap;
+    requires evolution.gene;
+    requires evolution.model;
+    requires common.util;
+    requires io.jenetics.base;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires slf4j.api;
+    requires common.constants;
+    requires iclij.common.model;
+    requires common.config;
+    requires org.apache.commons.lang3;
+    requires jackson.annotations;
+}
