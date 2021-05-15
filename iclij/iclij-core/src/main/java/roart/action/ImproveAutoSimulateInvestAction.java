8b9a9cc5d24af63d8dbb6757cd71152c1ae56a4f
Roar Thronæs <roart@nvg.ntnu.no>
2021-05-15 15:42:44 +0200

    Paralell adviser algorithm using sim feedback, temp commit (I250).

---
+++ iclij/iclij-core/src/main/java/roart/action/ImproveAutoSimulateInvestAction.java
@@ -0,0 +1,113 @@
+package roart.action;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import roart.action.MarketAction.MarketComponentTime;
+import roart.common.constants.Constants;
+import roart.component.Component;
+import roart.component.model.ComponentData;
+import roart.iclij.config.IclijConfig;
+import roart.iclij.config.Market;
+import roart.iclij.filter.Memories;
+import roart.iclij.model.IncDecItem;
+import roart.iclij.model.MLMetricsItem;
+import roart.iclij.model.MemoryItem;
+import roart.iclij.model.Parameters;
+import roart.iclij.model.WebData;
+import roart.iclij.model.action.ImproveAutoSimulateInvestActionData;
+import roart.iclij.model.action.ImproveSimulateInvestActionData;
+import roart.service.model.ProfitData;
+
+public class ImproveAutoSimulateInvestAction extends MarketAction {
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public ImproveAutoSimulateInvestAction() {
+        setActionData(new ImproveAutoSimulateInvestActionData());
+    }
+    
+    @Override
+    protected List<IncDecItem> getIncDecItems() {
+        return null;
+    }
+
+    @Override
+    protected List getAnArray() {
+        return null;
+    }
+
+    @Override
+    protected Boolean getBool() {
+        return null;
+    }
+
+    @Override
+    protected boolean getEvolve(Component component, ComponentData param) {
+        return true;
+    }
+
+    @Override
+    protected List<MemoryItem> getMemItems(MarketComponentTime marketTime, WebData myData, ComponentData param,
+            IclijConfig config, Boolean evolve, Map<String, ComponentData> dataMap) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    protected LocalDate getPrevDate(ComponentData param, Market market) {
+        return param.getInput().getEnddate();
+    }
+
+    @Override
+    public void setValMap(ComponentData param) {
+        param.getAndSetWantedCategoryValueMap();
+    }
+
+    @Override
+    protected void handleComponent(MarketAction action, Market market, ProfitData profitdata, ComponentData param,
+            Memories listComponent, Map<String, Component> componentMap, Map<String, ComponentData> dataMap,
+            Boolean buy, String subcomponent, WebData myData, IclijConfig config, Parameters parameters,
+            boolean wantThree, List<MLMetricsItem> mlTests) {
+        if (param.getUpdateMap() == null) {
+            param.setUpdateMap(new HashMap<>());
+        }
+        param.getInput().setDoSave(false);
+
+        try {
+            param.setFuturedays(0);
+            param.setOffset(0);
+            param.setDates(null, null, action.getActionData(), market);
+        } catch (ParseException e) {
+            log.error(Constants.EXCEPTION, e);
+        }
+        //List<MemoryItem> memories = findAllMarketComponentsToCheckNew(myData, param, 0, config, false, dataMap, componentMap, subcomponent, parameters, market);
+        
+        for (Entry<String, Component> entry : componentMap.entrySet()) {
+            Component component = entry.getValue();
+            if (component == null) {
+                continue;
+            }
+            
+            boolean evolve = false; // param.getInput().getConfig().wantEvolveML();
+            //component.set(market, param, profitdata, positions, evolve);
+            //ComponentData componentData = component.handle(market, param, profitdata, positions, evolve, new HashMap<>());
+            // 0 ok?
+
+            ComponentData componentData = component.improve(action, param, market, profitdata, listComponent, evolve, subcomponent, parameters, false, mlTests);
+            Map<String, Object> updateMap = componentData.getUpdateMap();
+            if (updateMap != null) {
+                param.getUpdateMap().putAll(updateMap);
+            }
+
+        }
+
+    }
+}
