78a9f5477935b4e70590c6a6da338f160250035d
Roar Thronæs <roart@nvg.ntnu.no>
2022-03-06 23:45:41 +0100

    Initial framework for file save access (I286).

---
+++ common/constants/src/main/java/roart/common/constants/FileSystemConstants.java
@@ -0,0 +1,30 @@
+package roart.common.constants;
+
+public class FileSystemConstants {
+
+    public enum FileSystemType { LOCAL, SWIFT, HDFS, S3 };
+    
+    public static final String LOCALTYPE = "local";
+    public static final String HDFSTYPE = "hdfs";
+    public static final String SWIFTTYPE = "swift";
+    public static final String S3TYPE = "s3";
+    
+    /*
+    public static final String LOCAL = "local:";
+    public static final String FILE = "file:";
+    public static final int FILELEN = 5;
+    public static final String HDFS = "hdfs:";
+    public static final int HDFSLEN = 5;
+    public static final String SWIFT = "swift:";
+    public static final int SWIFTLEN = 6;
+    public static final String S3 = "s3:";
+    public static final String FILESLASH = "file://";
+    public static final int FILESLASHLEN = 7;
+    public static final String HDFSSLASH = "hdfs://";
+    public static final int HDFSSLASHLEN = 7;
+    public static final String SWIFTSLASH = "swift://";
+    public static final int SWIFTSLASHLEN = 8;
+    public static final String DOUBLESLASH = "//";
+    public static final int DOUBLESLASHLEN = 2;
+    */
+}
