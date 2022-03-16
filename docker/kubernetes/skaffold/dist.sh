083725b2cd942e28275d08eac1bea38ea9282dc4
Roar Thronæs <roart@nvg.ntnu.no>
2022-03-16 22:41:56 +0100

    Mostly changes for using prebuilt, for automation (I155).

---
+++ docker/kubernetes/skaffold/dist.sh
@@ -0,0 +1,15 @@
+export DISTLIB=distribution/target/stockstat-distribution-0.6-SNAPSHOT-bin/stockstat-distribution-0.6-SNAPSHOT/lib
+export COREJAR=stockstat-core-0.6-SNAPSHOT.jar
+export HDFSJAR=stockstat-hdfs-0.6-SNAPSHOT.jar
+export ICOREJAR=stockstat-iclij-core-0.6-SNAPSHOT.jar
+export EVOLVEJAR=stockstat-iclij-evolve-0.6-SNAPSHOT.jar
+export SIMJAR=stockstat-iclij-sim-0.6-SNAPSHOT.jar
+export IWEBCOREJAR=stockstat-iclij-webcore-0.6-SNAPSHOT.jar
+export INPUTJAR=tockstat-input-0.6-SNAPSHOT-jar-with-dependencies.jar
+export S3JAR=stockstat-s3-0.6-SNAPSHOT.jar
+export SWIFTJAR=stockstat-swift-0.6-SNAPSHOT.jar
+
+export WEBRDOCROOT=distribution/target/stockstat-distribution-0.6-SNAPSHOT-bin/stockstat-distribution-0.6-SNAPSHOT/webr/docroot/
+export IWEBRDOCROOT=distribution/target/stockstat-distribution-0.6-SNAPSHOT-bin/stockstat-distribution-0.6-SNAPSHOT/webr/docroot/
+export WEBADIST=weba/dist/
+export IWEBADIST=iclij/iclij-weba/dist/
