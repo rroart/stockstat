d2a25602b025fb8e85ade47541d44fdcc632eab9
Roar Thronæs <roart@nvg.ntnu.no>
2022-04-23 22:35:09 +0200

    Python3 in Docker, for automation (I155).

--- pytorch/flaskpt.sh
+++ pytorch/flaskpt.sh
@@ -3,7 +3,7 @@
 eval "$(conda shell.bash hook)"
 conda activate gem
 
-PYTHON3=python
+PYTHON3=python3
 [ -z "$PYTHON3" ] && PYTHON3=`command -v python3.5`
 [ -z "$PYTHON3" ] && PYTHON3=`command -v python3.4`
 [ -z "$PYTHON3" ] && PYTHON3=`command -v python3`
