d317eca5021dab72ad85026d582f719c91e4e659
Roar Thronæs <roart@nvg.ntnu.no>
2023-04-24 21:34:23 +0200

    Remove private config (I275).

--- config/ievolve/localdev.sh
+++
@@ -1,7 +0,0 @@
-export HIBERNATE=false
-export IEVOLVETMPL=/home/roart/src/stockstatspark/config/ievolve/ievolve.xml.tmpl
-export CACHE=true
-export CACHETTL=600
-export MYSERVICESIEVOLVE="{ \"evolvefilterevolve\" : \"\", \"evolvefilterprofit\" : \"\", \"evolvefilterfilter\" : \"\", \"evolvefilterabovebelow\" : \"\" }"
-#export SERVICES="{ \"filterinvest\" : \"spring\" }"
-#export COMMUNICATIONS="{ \"spring\" : \"localhost\" }"
