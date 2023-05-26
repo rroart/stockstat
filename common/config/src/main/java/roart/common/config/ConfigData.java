72ddb9d076b86861f47f022c8caf4325bd165daf
Roar Thronæs <roart@nvg.ntnu.no>
2023-05-26 20:46:19 +0200

    Config, common, fixed and non-fixed (I275).

---
+++ common/config/src/main/java/roart/common/config/ConfigData.java
@@ -0,0 +1,91 @@
+package roart.common.config;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigData {
+
+    private ConfigMaps configMaps;
+
+    private ConfigTreeMap configTreeMap;
+    
+    private Map<String, Object> configValueMap;
+    
+    private LocalDate date;
+    
+    private String market;
+    
+    private String mlmarket;
+
+    private boolean dataset = false;
+
+    public ConfigMaps getConfigMaps() {
+        return configMaps;
+    }
+
+    public void setConfigMaps(ConfigMaps configMaps) {
+        this.configMaps = configMaps;
+    }
+
+    public ConfigTreeMap getConfigTreeMap() {
+        return configTreeMap;
+    }
+
+    public void setConfigTreeMap(ConfigTreeMap configTreeMap) {
+        this.configTreeMap = configTreeMap;
+    }
+
+    public Map<String, Object> getConfigValueMap() {
+        return configValueMap;
+    }
+
+    public void setConfigValueMap(Map<String, Object> configValueMap) {
+        this.configValueMap = configValueMap;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public String getMlmarket() {
+        return mlmarket;
+    }
+
+    public void setMlmarket(String mlmarket) {
+        this.mlmarket = mlmarket;
+    }
+
+    public boolean isDataset() {
+        return dataset;
+    }
+
+    public void setDataset(boolean dataset) {
+        this.dataset = dataset;
+    }
+
+    public ConfigData copy() {
+        ConfigData data = new ConfigData();
+        data.setConfigValueMap(new HashMap<>(getConfigValueMap()));
+        data.setConfigMaps(configMaps);
+        data.setConfigTreeMap(configTreeMap);
+        data.setDataset(dataset);
+        data.setDate(date);
+        data.setMarket(market);
+        data.setMlmarket(mlmarket);
+        return data;
+    }
+    
+}
