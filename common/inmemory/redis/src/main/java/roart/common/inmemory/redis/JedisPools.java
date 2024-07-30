e894d6f0b66666c28045ae5d397503337d15b49c
Roar Thronæs <roart@nvg.ntnu.no>
2024-07-30 22:34:58 +0200

    Queue fix, for concurrency (I314).

--- common/inmemory/redis/src/main/java/roart/common/inmemory/redis/JedisPools.java
+++ common/inmemory/redis/src/main/java/roart/common/inmemory/redis/JedisPools.java
@@ -1,13 +1,13 @@
 package roart.common.inmemory.redis;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import redis.clients.jedis.JedisPool;
 
 public class JedisPools {
  
-    private static Map<String, JedisPool> map = new HashMap<>();
+    private static Map<String, JedisPool> map = new ConcurrentHashMap<>();
 
     public static JedisPool get(String server) {
         map.computeIfAbsent(server, v -> new JedisPool(server));
