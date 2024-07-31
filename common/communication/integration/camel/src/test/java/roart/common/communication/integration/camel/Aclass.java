3bbcfb8f66d6d64fdb8eeb7f2676cefb84e2c923
Roar Thronæs <roart@nvg.ntnu.no>
2024-07-31 23:23:20 +0200

    Fixes for communication requeue if service crashed (I311).

---
+++ common/communication/integration/camel/src/test/java/roart/common/communication/integration/camel/Aclass.java
@@ -0,0 +1,26 @@
+package roart.common.communication.integration.camel;
+
+public class Aclass {
+    public String getS() {
+        return s;
+    }
+    public void setS(String s) {
+        this.s = s;
+    }
+    public Integer getI() {
+        return i;
+    }
+    public void setI(Integer i) {
+        this.i = i;
+    }
+    private String s;
+    private Integer i;
+    public Aclass(String s, Integer i) {
+        this.s = s;
+        this.i = i;
+    }
+    public Aclass() {
+        
+    }
+}
+
