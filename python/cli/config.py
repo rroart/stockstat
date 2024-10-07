01f42ce4f5e740ca23a62b26af23677f49deb8cb
Roar Thronæs <roart@nvg.ntnu.no>
2024-10-07 22:29:17 +0200

    Gan for generative ML (I316).

--- python/cli/config.py
+++ python/cli/config.py
@@ -15,3 +15,46 @@ TENSORFLOWGRUCONFIG = { 'name' : 'gru', 'steps' : 1000, 'hidden' : 100, 'layers'
 TENSORFLOWLIRCONFIG = { 'name' : 'lir', 'steps' : 1000, 'lr' : 0.01 }
 TENSORFLOWDCGANCONFIG = { 'name' : 'dcgan', 'steps' : 20, 'lr' : 0.0001 }
 TENSORFLOWCONDITIONALGANCONFIG = { 'name' : 'conditionalgan', 'steps' : 20, 'lr' : 0.0003 }
+
+def get(cf):
+    if cf == 'tensorflowDNNConfig':
+        return cf, 1, TENSORFLOWDNNCONFIG
+    elif cf == 'tensorflowLICConfig':
+        return cf, 2, TENSORFLOWLICCONFIG
+    elif cf == 'tensorflowMLPConfig':
+        return cf, 3, TENSORFLOWMLPCONFIG
+    elif cf == 'tensorflowRNNConfig':
+        return cf, 4, TENSORFLOWRNNCONFIG
+    elif cf == 'tensorflowCNNConfig':
+        return cf, 5, TENSORFLOWCNNCONFIG
+    elif cf == 'tensorflowLSTMConfig':
+        return cf, 6, TENSORFLOWLSTMCONFIG
+    elif cf == 'tensorflowGRUConfig':
+        return cf, 7, TENSORFLOWGRUCONFIG
+    elif cf == 'tensorflowLIRConfig':
+        return cf, 8, TENSORFLOWLIRCONFIG
+    elif cf == 'tensorflowCNN2Config':
+        return cf, 9, TENSORFLOWCNN2CONFIG
+    elif cf == 'tensorflowQNNConfig':
+        return cf, 10, TENSORFLOWQNNCONFIG
+    elif cf == 'tensorflowQCNNConfig':
+        return cf, 11, TENSORFLOWQCNNCONFIG
+    elif cf == 'tensorflowConditionalGANConfig':
+        return cf, 12, TENSORFLOWCONDITIONALGANCONFIG
+    elif cf == 'tensorflowDCGANConfig':
+        return cf, 13, TENSORFLOWDCGANCONFIG
+    elif cf == 'tensorflowNeuralStyleTransferConfig':
+        return cf, 14, TENSORFLOWNEURALSTYLETRANSFERCONFIG
+    elif cf == 'pytorchMLPConfig':
+        return cf, 1, PYTORCHMLPCONFIG
+    elif cf == 'pytorchRNNConfig':
+        return cf, 2, PYTORCHRNNCONFIG
+    elif cf == 'pytorchLSTMConfig':
+        return cf, 3, PYTORCHLSTMCONFIG
+    elif cf == 'pytorchGRUConfig':
+        return cf, 4, PYTORCHGRUCONFIG
+    elif cf == 'pytorchCNNConfig':
+        return cf, 5, PYTORCHCNNCONFIG
+    elif cf == 'pytorchCNN2Config':
+        return cf, 6, PYTORCHCNN2CONFIG
+    return None
