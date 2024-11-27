b1666a9c877960dbf5beac0b3d9c2b39d931e29e
Roar Thronæs <roart@nvg.ntnu.no>
2024-11-27 22:12:49 +0100

    New pipeline serialization (I309).

---
+++ common/model/src/main/java/roart/common/pipeline/data/SerialDouble.java
@@ -0,0 +1,18 @@
+package roart.common.pipeline.data;
+
+public class SerialDouble extends SerialObject {
+    public Double adouble;
+
+    public Double getAdouble() {
+        return adouble;
+    }
+
+    public void setAdouble(Double adouble) {
+        this.adouble = adouble;
+    }
+
+    public SerialDouble() {
+        super();
+    }
+    
+}
---
+++ common/model/src/main/java/roart/common/pipeline/data/SerialInteger.java
@@ -0,0 +1,17 @@
+package roart.common.pipeline.data;
+
+public class SerialInteger extends SerialObject {
+    public Integer integer;
+
+    public SerialInteger() {
+        super();
+    }
+
+    public Integer getInteger() {
+        return integer;
+    }
+
+    public void setInteger(Integer integer) {
+        this.integer = integer;
+    }
+}
