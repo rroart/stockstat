e39e4299fa21bbbec3d6ad512c18fd5c365e99e8
Roar Thronæs <roart@nvg.ntnu.no>
2025-10-07 22:21:22 +0200

    Temp commit, more functionality, ML model changes (I325).

--- common/evolution/gene/src/main/java/roart/gene/ml/impl/PytorchConfigGene.java
+++ common/evolution/gene/src/main/java/roart/gene/ml/impl/PytorchConfigGene.java
@@ -143,8 +143,9 @@ public abstract class PytorchConfigGene extends NeuralNetConfigGene {
     }
     
     protected String generateLoss() {
-        String[] losses = { "mse", "nllloss", "crossentropyloss", "l1loss", "smoothl1loss", "poissonloss" };
-        if (predictor) {
+        //String[] losses = { "mse", "nllloss", "crossentropyloss", "l1loss", "smoothl1loss", "poissonloss" };
+        String[] losses = { "l1", "mse", "cross_entropy", "ctc", "nl", "poissonnl", "gaussiannl", "kl", "bce", "bcewithlogits", "marginrank", "hingeembedding", "multilabelmargin", "multilabelsoftmargin", "cosineembedding", "multimargin", "tripletmargin",  "tripletmarginwithdistance" };
+                if (predictor) {
             losses = new String[] { "mse", "l1loss", "smoothl1loss", "poissonloss" };
         }
         return losses[random.nextInt(losses.length)];
