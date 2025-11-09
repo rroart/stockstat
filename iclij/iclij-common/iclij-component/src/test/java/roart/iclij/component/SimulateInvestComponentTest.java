933914d3f393adff57e281253f6d5c4beac83ee8
Roar Thronæs <roart@nvg.ntnu.no>
2025-11-09 21:42:12 +0100

    Temp commit, tests for simulate invest action (I220).

--- iclij/iclij-common/iclij-component/src/test/java/roart/iclij/component/SimulateInvestComponentTest.java
+++ iclij/iclij-common/iclij-component/src/test/java/roart/iclij/component/SimulateInvestComponentTest.java
@@ -3,6 +3,7 @@ package roart.iclij.component;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -72,7 +73,7 @@ public class SimulateInvestComponentTest {
         Results aResult = new SimulateInvestComponent().new Results();
         aResult.plotCapital = new ArrayList(List.of(1.0));
         aResult.stockhistory = new ArrayList(List.of(new SimulateStock("1", 100.1, 3, 99.1, 101.1, LocalDate.now(), LocalDate.now(), 0, "ok")));
-        aResult.history = new ArrayList(List.of(new StockHistory("2022.02.02", capital, capital, 0.2, "0.3", List.of("EQ"), "trend", 0)));
+        aResult.history = new ArrayList(List.of(new StockHistory("2022.02.02", capital, capital, 0.2, "0.3", List.of("EQ"), null, null, "trend", 0)));
         //Map<String, Object> map = new HashMap<>();
         //map.put(SimConstants.STOCKHISTORY, aResult.stockhistory);
         //map.put(SimConstants.HISTORY, new SerialListStockHistory(aResult.history));
@@ -188,4 +189,101 @@ public class SimulateInvestComponentTest {
         return data2;
     }
 
-}
+    @Test
+    public void test2() {
+        
+    }
+    
+    @Test
+    public void testHelperMethodsReflection() throws Exception {
+        SimulateInvestComponent comp = new SimulateInvestComponent();
+
+        // test myAddAll and getText via reflection
+        java.lang.reflect.Method myAddAll = SimulateInvestComponent.class.getDeclaredMethod("myAddAll", List.class, List.class);
+        myAddAll.setAccessible(true);
+        SimulateStock s1 = new SimulateStock(); s1.setId("A");
+        SimulateStock s2 = new SimulateStock(); s2.setId("B");
+        List<SimulateStock> main = new ArrayList<>(); main.add(s1);
+        List<SimulateStock> toAdd = new ArrayList<>(); toAdd.add(s1); toAdd.add(s2);
+        myAddAll.invoke(comp, main, toAdd);
+        // now size should be 2
+        assertEquals(2, main.size());
+
+        java.lang.reflect.Method getText = SimulateInvestComponent.class.getDeclaredMethod("getText", Map.class, List.class);
+        getText.setAccessible(true);
+        Map<String,String> stocks = new HashMap<>(); stocks.put("A","Alpha"); stocks.put("B","Beta");
+        String txt = (String) getText.invoke(comp, stocks, List.of("A","B"));
+        assertTrue(txt.contains("Alpha") && txt.contains("Beta"));
+
+        // test index offset helpers
+        SimulateInvestComponent.Mydate mydate = comp.new Mydate(); mydate.indexOffset = 10; mydate.prevIndexOffset = 5;
+        SimulateInvestConfig simConfig = new SimulateInvestConfig();
+        simConfig.setExtradelay(2);
+        simConfig.setDelay(3);
+        java.lang.reflect.Method getValueIndexOffset = SimulateInvestComponent.class.getDeclaredMethod("getValueIndexOffset", SimulateInvestComponent.Mydate.class, SimulateInvestConfig.class);
+        getValueIndexOffset.setAccessible(true);
+        int val = (Integer) getValueIndexOffset.invoke(comp, mydate, simConfig);
+        assertEquals(12, val);
+        java.lang.reflect.Method getBSIndexOffset = SimulateInvestComponent.class.getDeclaredMethod("getBSIndexOffset", SimulateInvestComponent.Mydate.class, SimulateInvestConfig.class);
+        getBSIndexOffset.setAccessible(true);
+        int bs = (Integer) getBSIndexOffset.invoke(comp, mydate, simConfig);
+        assertEquals(5, bs);
+        java.lang.reflect.Method getBSValueIndexOffset = SimulateInvestComponent.class.getDeclaredMethod("getBSValueIndexOffset", SimulateInvestComponent.Mydate.class, SimulateInvestConfig.class);
+        getBSValueIndexOffset.setAccessible(true);
+        int bsv = (Integer) getBSValueIndexOffset.invoke(comp, mydate, simConfig);
+        assertEquals(8, bsv);
+
+        // test getBuyList/getSellList/getSum
+        Map<String, List<List<Double>>> category = new HashMap<>();
+        List<Double> prices = List.of(1.0,2.0,3.0,4.0);
+        category.put("A", List.of(prices));
+        category.put("B", List.of(prices));
+        category.put("C", List.of(prices));
+        java.lang.reflect.Method getBuyList = SimulateInvestComponent.class.getDeclaredMethod("getBuyList", Map.class, Set.class, Integer.class);
+        getBuyList.setAccessible(true);
+        @SuppressWarnings("unchecked")
+        List<SimulateStock> newbuys = (List<SimulateStock>) getBuyList.invoke(comp, category, new LinkedHashMap<String,Integer>(){{put("A",1);put("B",1);}}.keySet(), 2);
+        assertEquals(2, newbuys.size());
+
+        // prepare mystocks for sell and sum
+        SimulateStock owned1 = new SimulateStock(); owned1.setId("A"); owned1.setPrice(5.0); owned1.setCount(2);
+        SimulateStock owned2 = new SimulateStock(); owned2.setId("B"); owned2.setPrice(3.0); owned2.setCount(3);
+        List<SimulateStock> mystocks = new ArrayList<>(); mystocks.add(owned1); mystocks.add(owned2);
+        java.lang.reflect.Method getSellList = SimulateInvestComponent.class.getDeclaredMethod("getSellList", List.class, List.class);
+        getSellList.setAccessible(true);
+        @SuppressWarnings("unchecked")
+        List<SimulateStock> sells = (List<SimulateStock>) getSellList.invoke(comp, mystocks, newbuys);
+        // newbuys likely contains A,B so sells should be empty
+        assertEquals(0, sells.size());
+
+        java.lang.reflect.Method getSum = SimulateInvestComponent.class.getDeclaredMethod("getSum", List.class);
+        getSum.setAccessible(true);
+        double sum = (Double) getSum.invoke(comp, mystocks);
+        assertEquals(5.0*2 + 3.0*3, sum, 1e-9);
+
+        // test increase/update/stoploss run without exception
+        Map<String, List<List<Double>>> cat2 = new HashMap<>();
+        List<Double> p2 = List.of(10.0,11.0,12.0,13.0,14.0);
+        cat2.put("X", List.of(p2));
+        SimulateStock s = new SimulateStock(); s.setId("X"); List<SimulateStock> owned = new ArrayList<>(); owned.add(s);
+        java.lang.reflect.Method increase = SimulateInvestComponent.class.getDeclaredMethod("increase", List.class, int.class, Map.class, int.class, int.class);
+        increase.setAccessible(true);
+        double inc = (Double) increase.invoke(comp, owned, 1, cat2, 2, 0);
+        assertEquals(14.0/13.0, inc, 1e-9);
+        java.lang.reflect.Method update = SimulateInvestComponent.class.getDeclaredMethod("update", Map.class, List.class, int.class, List.class, int.class, int.class);
+        update.setAccessible(true);
+        List<SimulateStock> noc = new ArrayList<>();
+        int up = (Integer) update.invoke(comp, cat2, owned, 1, noc, 2, 0);
+        assertEquals(1, up);
+
+        java.lang.reflect.Method createMap = SimulateInvestComponent.class.getDeclaredMethod("createStockDatesBiMap", List.class);
+        createMap.setAccessible(true);
+        @SuppressWarnings("unchecked")
+        com.google.common.collect.BiMap<String, LocalDate> bi = (com.google.common.collect.BiMap<String, LocalDate>) createMap.invoke(comp, List.of("2020.01.01","2020.01.02","2020.01.03","2020.01.04","2020.01.05"));
+        java.lang.reflect.Method stoploss = SimulateInvestComponent.class.getDeclaredMethod("stoploss", List.class, List.class, int.class, Map.class, int.class, List.class, double.class, String.class, com.google.common.collect.BiMap.class, int.class);
+        stoploss.setAccessible(true);
+        List<SimulateStock> sells2 = new ArrayList<>();
+        stoploss.invoke(comp, owned, List.of("2020.01.01","2020.01.02","2020.01.03","2020.01.04","2020.01.05"), 1, cat2, 1, sells2, 0.5, "STOP", bi, 0);
+        // ensure sells2 is a list (may be empty), no exception
+        assertNotEquals(null, sells2);
+    }
