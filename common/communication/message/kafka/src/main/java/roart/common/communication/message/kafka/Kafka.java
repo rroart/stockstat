cbb1cf4ed5e8362200add3462df4717095284ce3
Roar Thronæs <roart@nvg.ntnu.no>
2026-05-12 22:49:41 +0200

    Use more defaults, for Communication (I232).

--- common/communication/message/kafka/src/main/java/roart/common/communication/message/kafka/Kafka.java
+++ common/communication/message/kafka/src/main/java/roart/common/communication/message/kafka/Kafka.java
@@ -64,11 +64,11 @@ public class Kafka extends MessageCommunication {
             Properties props = new Properties();
             props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, connection); // List of brokers that the producer asks to get the topic leader
             // TODO
-            props.put(ProducerConfig.ACKS_CONFIG, "all");
-            props.put(ProducerConfig.RETRIES_CONFIG, 0);
-            props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-            props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-            props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+            // same as default props.put(ProducerConfig.ACKS_CONFIG, "all");
+            // 2147483647 default props.put(ProducerConfig.RETRIES_CONFIG, 0);
+            // same as default props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+            // 5 default props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+            // same as defualt props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
             
             props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, 
                StringSerializer.class.getName());
@@ -86,9 +86,9 @@ public class Kafka extends MessageCommunication {
             
             props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, connection);
             props.put(ConsumerConfig.GROUP_ID_CONFIG, "test");
-            props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, auto_commit);
-            props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-            props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+            props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, auto_commit); // true default
+            // 5000 default props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+            // 45000 default props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
             props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, 
                     StringDeserializer.class.getName());
             props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, 
@@ -97,6 +97,7 @@ public class Kafka extends MessageCommunication {
             //props.put(ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG, MSGSIZE);
             if (sendreceive) {
                 props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+                // default is latest
             }
             consumer = new KafkaConsumer<>(props);
 
