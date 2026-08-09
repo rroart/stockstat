c5a5b891d294f7c62fe6f79efc17e6e7e8ea2166
Roar Thronæs <roart@nvg.ntnu.no>
2026-08-09 22:53:46 +0200

    Gateway (I334).

---
+++ gateway/src/test/java/roart/gateway/ApplicationTest.java
@@ -0,0 +1,67 @@
+package roart.gateway;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.Test;
+import org.wiremock.spring.ConfigureWireMock;
+import org.wiremock.spring.EnableWireMock;
+import org.wiremock.spring.InjectWireMock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Ryan Baxter
+ */
+// tag::code[]
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@EnableWireMock({
+	@ConfigureWireMock(name = "httpbin", baseUrlProperties = "httpbin")
+})
+public class ApplicationTest {
+
+	@InjectWireMock("httpbin")
+	private WireMockServer httpbin;
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private WebTestClient webClient;
+
+	@Test
+	void contextLoads() {
+		this.httpbin.stubFor(get(urlEqualTo("/get"))
+				.willReturn(aResponse()
+					.withBody("{\"headers\":{\"Hello\":\"World\"}}")
+					.withHeader("Content-Type", "application/json")));
+		this.httpbin.stubFor(get(urlEqualTo("/delay/3"))
+			.willReturn(aResponse()
+				.withBody("no fallback")
+				.withFixedDelay(3000)));
+
+		this.webClient
+			.get().uri("/get")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.headers.Hello").isEqualTo("World");
+		this.webClient
+			.get().uri("/delay/3")
+			.header("Host", "www.circuitbreaker.com")
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.consumeWith(
+				response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
+	}
+}
+// end::code[]
